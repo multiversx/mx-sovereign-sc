b3893a240de788c92bc8e41507c45e07055e0155
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2023-10-11 11:02:42 +0300

    build

--- esdt-safe/wasm/src/lib.rs
+++ esdt-safe/wasm/src/lib.rs
@@ -5,12 +5,15 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           31
+// Endpoints:                           23
 // Async Callback (empty):               1
-// Total number of exported functions:  33
+// Total number of exported functions:  25
 
 #![no_std]
-#![feature(alloc_error_handler, lang_items)]
+
+// Configuration that works with rustc < 1.73.0.
+// TODO: Recommended rustc version: 1.73.0 or newer.
+#![feature(lang_items)]
 
 multiversx_sc_wasm_adapter::allocator!();
 multiversx_sc_wasm_adapter::panic_handler!();
@@ -18,38 +21,31 @@ multiversx_sc_wasm_adapter::panic_handler!();
 multiversx_sc_wasm_adapter::endpoints! {
     esdt_safe
     (
-        setTransactionBatchStatus
-        addRefundBatch
-        createTransaction
-        claimRefund
-        getRefundAmounts
-        setFeeEstimatorContractAddress
-        setEthTxGasLimit
-        setDefaultPricePerGasUnit
-        setTokenTicker
-        calculateRequiredFee
-        getFeeEstimatorContractAddress
-        getDefaultPricePerGasUnit
-        getEthTxGasLimit
-        distributeFees
-        addTokenToWhitelist
-        removeTokenFromWhitelist
-        getAllKnownTokens
-        getAccumulatedTransactionFees
-        setMaxTxBatchSize
-        setMaxTxBatchBlockDuration
-        getCurrentTxBatch
-        getFirstBatchAnyStatus
-        getBatch
-        getBatchStatus
-        getFirstBatchId
-        getLastBatchId
-        setMaxBridgedAmount
-        getMaxBridgedAmount
-        pause
-        unpause
-        isPaused
+        init => init
+        upgrade => upgrade
+        setTransactionBatchStatus => set_transaction_batch_status
+        addRefundBatch => add_refund_batch
+        createTransaction => create_transaction
+        claimRefund => claim_refund
+        getRefundAmounts => get_refund_amounts
+        getSovereignTxGasLimit => sovereign_tx_gas_limit
+        addTokenToWhitelist => add_token_to_whitelist
+        removeTokenFromWhitelist => remove_token_from_whitelist
+        getAllKnownTokens => token_whitelist
+        setMaxTxBatchSize => set_max_tx_batch_size
+        setMaxTxBatchBlockDuration => set_max_tx_batch_block_duration
+        getCurrentTxBatch => get_current_tx_batch
+        getFirstBatchAnyStatus => get_first_batch_any_status
+        getBatch => get_batch
+        getBatchStatus => get_batch_status
+        getFirstBatchId => first_batch_id
+        getLastBatchId => last_batch_id
+        setMaxBridgedAmount => set_max_bridged_amount
+        getMaxBridgedAmount => max_bridged_amount
+        pause => pause_endpoint
+        unpause => unpause_endpoint
+        isPaused => paused_status
     )
 }
 
-multiversx_sc_wasm_adapter::empty_callback! {}
+multiversx_sc_wasm_adapter::async_callback_empty! {}
