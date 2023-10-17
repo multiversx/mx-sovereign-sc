474d2b6a8e188b16215e8792783450e728a53346
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2023-10-17 12:05:00 +0300

    other test

--- esdt-safe/tests/bridge_test.rs
+++ esdt-safe/tests/bridge_test.rs
@@ -21,6 +21,7 @@ use multiversx_sc_scenario::{
 use transaction::{
     transaction_status::TransactionStatus, StolenFromFrameworkEsdtTokenData, Transaction,
 };
+use tx_batch_module::TxBatchModule;
 
 mod bridge_setup;
 
@@ -264,4 +265,86 @@ fn transfer_token_to_and_from_sov_ok() {
         &rust_biguint!(TOKEN_BALANCE - 2_000 + 500),
         Some(&DummyAttributes { dummy: 42 }),
     );
+}
+
+#[test]
+fn transfer_token_from_sov_no_roles_refund() {
+    let mut bridge_setup = BridgeSetup::new(esdt_safe::contract_obj);
+    let user_addr = bridge_setup.user.clone();
+    let dest = bridge_setup.sov_dest_addr.clone();
+
+    bridge_setup
+        .b_mock
+        .execute_tx(
+            &bridge_setup.owner,
+            &bridge_setup.bridge_wrapper,
+            &rust_biguint!(0),
+            |sc| {
+                let mut tokens = ManagedVec::new();
+                tokens.push(EsdtTokenPayment::new(
+                    managed_token_id!(FUNGIBLE_TOKEN_ID),
+                    0,
+                    managed_biguint!(500),
+                ));
+                tokens.push(EsdtTokenPayment::new(
+                    managed_token_id!(NFT_TOKEN_ID),
+                    1,
+                    managed_biguint!(500),
+                ));
+
+                let mut token_data = ManagedVec::new();
+                token_data.push(StolenFromFrameworkEsdtTokenData::default());
+                token_data.push(StolenFromFrameworkEsdtTokenData::default());
+
+                let mut transfers = MultiValueEncoded::new();
+                transfers.push(Transaction {
+                    block_nonce: 1,
+                    nonce: 1,
+                    from: managed_address!(&dest),
+                    to: managed_address!(&user_addr),
+                    tokens,
+                    token_data,
+                    opt_transfer_data: None,
+                    is_refund_tx: false,
+                });
+
+                sc.batch_transfer_esdt_token(
+                    1,
+                    BlsSignature::new_from_bytes(&DUMMY_SIG),
+                    transfers,
+                );
+            },
+        )
+        .assert_ok();
+
+    // user received no tokens
+    bridge_setup.b_mock.check_esdt_balance(
+        &bridge_setup.user,
+        FUNGIBLE_TOKEN_ID,
+        &rust_biguint!(TOKEN_BALANCE),
+    );
+    bridge_setup.b_mock.check_nft_balance(
+        &bridge_setup.user,
+        NFT_TOKEN_ID,
+        1,
+        &rust_biguint!(TOKEN_BALANCE),
+        Some(&DummyAttributes { dummy: 42 }),
+    );
+
+    // set block nonce in the future so batch is "final"
+    bridge_setup.b_mock.set_block_nonce(20);
+
+    bridge_setup
+        .b_mock
+        .execute_tx(
+            &bridge_setup.owner,
+            &bridge_setup.bridge_wrapper,
+            &rust_biguint!(0),
+            |sc| {
+                // transactions were converted into Elrond -> Sov for refunding
+                let opt_val = sc.get_current_tx_batch();
+                assert!(opt_val.is_some());
+            },
+        )
+        .assert_ok();
 }
