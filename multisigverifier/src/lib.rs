9aadcf606f4d7bdfd75d9e009fb89e7eeb9df04a
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-02-12 14:14:46 +0200

    Fixed errors and added utils module

--- multisigverifier/src/lib.rs
+++ multisigverifier/src/lib.rs
@@ -1,14 +1,17 @@
 mod bridge_operations;
-
+mod utils;
 // use user_role::UserRole;
 
 multiversx_sc::imports!();
 
 #[multiversx_sc::contract]
 pub trait Multisigverifier:
-   bridge_operations::BridgeOperationsModule
-   // + user_roles::UserRole
+   bls_signature::BlsSignatureModule
+   + bridge_operations::BridgeOperationsModule
+   + utils::UtilsModule
 {
     #[init]
-    fn init(&self) {}
+    fn init(&self, esdt_address: ManagedAddress) {
+      self.esdt_proxy_address().set(esdt_address);
+   }
 }
