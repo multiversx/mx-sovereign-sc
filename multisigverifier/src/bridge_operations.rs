7ef68eab789616218b1d703368a432a7c812a592
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-02-14 13:32:02 +0200

    Removed execution logic

--- multisigverifier/src/bridge_operations.rs
+++ multisigverifier/src/bridge_operations.rs
@@ -1,15 +1,11 @@
 use bls_signature::{self, BlsSignature};
-use esdt_safe::from_sovereign::{self, transfer_tokens::{self, ProxyTrait as _ }};
-use transaction::TransferData;
-
-use crate::utils::{self, UtilsModule};
 
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
 #[multiversx_sc::module]
-pub trait BridgeOperationsModule
-{
+pub trait BridgeOperationsModule {
+    
     #[endpoint(registerBridgeOps)]
     fn register_bridge_operations(
         &self,
@@ -17,49 +13,27 @@ pub trait BridgeOperationsModule
         hash_of_bridge_ops: ManagedBuffer,
         signature: BlsSignature<Self::Api>,
         signature_data: &ManagedBuffer,
-        //use Transaction nonce, token_data
     ) {
 
         let caller = self.blockchain().get_caller();
-        let is_batch_valid = self.check_validity(signature_data, &signature, caller);
+        let is_bls_valid = self.verify_bls_signature(signature_data, &signature, caller);
         
-        self.is_operations_batch_valid().insert(hash_of_hashes.clone(), is_batch_valid);
-        self.operations_mapper().insert(hash_of_hashes.clone(), hash_of_bridge_ops);
-        self.operations_signatures_mapper().insert(hash_of_hashes.clone(), signature);
-    }
-
-    #[only_owner]
-    #[endpoint(execBridgeOp)]
-    fn execute_bridge_operation_endpoint(
-        &self,
-        operation_hash: ManagedBuffer,
-        payments_list: MultiValueEncoded<EsdtTokenPayment<Self::Api>>,
-        transfer_data: TransferData<Self::Api>
-    ) {
-        // require action not executed
-        let caller = self.blockchain().get_caller();
-        let caller_id = self.bls_pub_keys().get_user_id(&caller);
-        let operation_validity = self.is_operations_batch_valid().get(&operation_hash).unwrap();
-
-        require!(
-            operation_validity,
-            "Operation was not signed"
-        );
-
-        for payment in payments_list {
-            self.execute_operation(&operation_hash, &payment, &transfer_data); 
+        if is_bls_valid {
+            self.operations_mapper().insert(hash_of_hashes.clone(), hash_of_bridge_ops);
         }
-
-        // transfer_tokens::batch_transfer_esdt_token();
     }
 
-    fn check_validity(
+    fn verify_bls_signature(
         &self,
         signature_data: &ManagedBuffer,
         signature: &BlsSignature<Self::Api>,
         user: ManagedAddress
     ) -> bool {
-        let is_bls_valid = self.crypto().verify_bls(user.as_managed_buffer(), signature_data, signature.as_managed_buffer());
+        let is_bls_valid = self.crypto().verify_bls(
+            user.as_managed_buffer(), 
+            signature_data, 
+            signature.as_managed_buffer()
+        );
         let signatures_count = self.signatures().get();
         let bls_pub_keys = self.bls_pub_keys().get_user_count() as u32;
 
@@ -69,23 +43,12 @@ pub trait BridgeOperationsModule
 
         false
     }
-   
-    fn execute_operation(&self, operation_hash: &ManagedBuffer, payment: &EsdtTokenPayment, data: &TransferData<Self::Api>) {
-        let esdt_address = self.esdt_proxy_address().get();
-        let esdt_proxy = utils::contract_obj().get_esdt_safe_proxy_instance(esdt_address);
-        let operation_signature = self.operations_signatures_mapper().get(operation_hash);
-        // esdt_proxy.batch_transfer_esdt_token(operation_hash, operation_signature, payment);
-    }
 
     #[storage_mapper("esdt_proxy_address")]
     fn esdt_proxy_address(&self) -> SingleValueMapper<ManagedAddress>;
 
     #[storage_mapper("isValid")]
     fn is_valid(&self) -> SingleValueMapper<bool>;
-
-    // maybe use a dictionary?
-    #[storage_mapper("valid_batches")]
-    fn is_operations_batch_valid(&self) -> MapMapper<ManagedBuffer, bool>;
 
     #[storage_mapper("board_members")]
     fn bls_pub_keys(&self) -> UserMapper; 
@@ -95,7 +58,4 @@ pub trait BridgeOperationsModule
 
     #[storage_mapper("operations_mapper")]
     fn operations_mapper(&self) -> MapMapper<ManagedBuffer, ManagedBuffer>;
-
-    #[storage_mapper("operations_signature")]
-    fn operations_signatures_mapper(&self) -> MapMapper<ManagedBuffer, BlsSignature<Self::Api>>;
 }
