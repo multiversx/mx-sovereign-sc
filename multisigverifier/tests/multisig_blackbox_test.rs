3adf59e584198bbafc24f6e96d64d448520fe560
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-03-05 15:44:04 +0200

    Modified test to fit register ops

--- multisigverifier/tests/multisig_blackbox_test.rs
+++ multisigverifier/tests/multisig_blackbox_test.rs
@@ -2,9 +2,15 @@ mod multisigverifier_setup;
 
 use bls_signature::BlsSignature;
 use multisigverifier::ProxyTrait;
-use multiversx_sc::{codec::multi_types::MultiValueVec, types::{Address, ManagedBuffer, ManagedByteArray, ManagedVec}};
-use multiversx_sc_scenario::{api::StaticApi, scenario_model::{Account, AddressValue, ScCallStep, ScDeployStep, SetStateStep}, ContractInfo, ScenarioWorld};
-use transaction::TransferData;
+use multiversx_sc::{
+    codec::multi_types::MultiValueVec,
+    types::{Address, ManagedBuffer, ManagedByteArray, ManagedVec},
+};
+use multiversx_sc_scenario::{
+    api::StaticApi,
+    scenario_model::{Account, AddressValue, ScCallStep, ScDeployStep, SetStateStep},
+    ContractInfo, ScenarioWorld,
+};
 
 const MULTISIG_PATH_EXPR: &str = "file:output/multisigverifier.wasm";
 const OWNER_ADDRESS_EXPR: &str = "address:owner";
@@ -39,21 +45,21 @@ impl MultisigTestState {
                 .put_account(OWNER_ADDRESS_EXPR, Account::new().nonce(1))
                 .new_address(OWNER_ADDRESS_EXPR, 1, MULTISIG_ADDRESS_EXPR)
                 .put_account(
-                    LEADER_ADDRESS_EXPR, 
+                    LEADER_ADDRESS_EXPR,
                     Account::new().nonce(1).balance(LEADER_ADDRESS_EXPR),
                 )
-                .put_account(VALIDATOR_ADDRESS_EXPR, Account::new().nonce(1))
+                .put_account(VALIDATOR_ADDRESS_EXPR, Account::new().nonce(1)),
         );
 
         let leader_address = AddressValue::from(LEADER_ADDRESS_EXPR).to_address();
-        let validator_address = AddressValue::from(VALIDATOR_ADDRESS_EXPR).to_address(); 
+        let validator_address = AddressValue::from(VALIDATOR_ADDRESS_EXPR).to_address();
         let multisig_contract = MultisigverifierContract::new(MULTISIG_ADDRESS_EXPR);
 
         Self {
             world,
             leader_address,
             validator_address,
-            multisig_contract
+            multisig_contract,
         }
     }
 
@@ -65,7 +71,7 @@ impl MultisigTestState {
             ScDeployStep::new()
                 .from(OWNER_ADDRESS_EXPR)
                 .code(multisig_code)
-                .call(self.multisig_contract.init(validators))
+                .call(self.multisig_contract.init(validators)),
         );
 
         self
@@ -74,17 +80,20 @@ impl MultisigTestState {
     fn propose_register_bridge_ops(
         &mut self,
         bridge_operations_hash: &str,
+        operations_hashes: ManagedVec<StaticApi, ManagedBuffer<StaticApi>>,
         signature: &bls_signature::BlsSignature<StaticApi>,
-        bridge_operations: MultiValueVec<TransferData<StaticApi>>
     ) {
-        self.world.sc_call_get_result(
-            ScCallStep::new().from(LEADER_ADDRESS_EXPR).call(
-                self.multisig_contract
-                    .register_bridge_operations(bridge_operations_hash, signature, bridge_operations)
-            )
-        )
+        self.world
+            .sc_call_get_result(ScCallStep::new().from(LEADER_ADDRESS_EXPR).call(
+                self.multisig_contract.register_bridge_operations(
+                    bridge_operations_hash,
+                    operations_hashes,
+                    signature,
+                ),
+            ))
     }
 }
+
 #[test]
 fn test_deploy() {
     let mut state = MultisigTestState::new();
@@ -101,21 +110,18 @@ fn test_register_bridge_ops() {
         b"EIZ2\x05\xf7q\xc7G\x96\x1f\xba0\xe2\xd1\xf5pE\x14\xd7?\xac\xff\x8d\x1a\x0c\x11\x900f5\xfb\xff4\x94\xb8@\xc5^\xc2,exn0\xe3\xf0\n"
     );
 
-    let first_transfer_data = TransferData {
-        args: ManagedVec::new(),
-        gas_limit: 50000,
-        function: ManagedBuffer::new()
-    }; 
+    let first_operation =
+        ManagedBuffer::from("95cdb166d6e12a8c4a783a48d2e4f647e15fac4e5a115d4483f95881630a5433");
+    let second_operation =
+        ManagedBuffer::from("4851cd6e4a4799ad0d8e8ead37c88d930874302ab11edcc60f608654be14b2ed");
 
-    let second_transfer_data = TransferData {
-        args: ManagedVec::new(),
-        gas_limit: 60000,
-        function: ManagedBuffer::new()
-    }; 
+    let mut bridge_operations = ManagedVec::new();
+    bridge_operations.push(first_operation);
+    bridge_operations.push(second_operation);
 
-    let mut bridge_operations = MultiValueVec::new();
-    bridge_operations.push(first_transfer_data);
-    bridge_operations.push(second_transfer_data);
-
-    let _ = state.propose_register_bridge_ops(bridge_operations_hash, &mock_signature, bridge_operations);
+    let _ = state.propose_register_bridge_ops(
+        bridge_operations_hash,
+        bridge_operations,
+        &mock_signature,
+    );
 }
