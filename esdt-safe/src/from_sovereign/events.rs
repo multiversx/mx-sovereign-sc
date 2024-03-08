0422fb7622566824b90615b1b1f8192504fdc357
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-03-08 11:19:55 +0200

    Fixed typo

--- esdt-safe/src/from_sovereign/events.rs
+++ esdt-safe/src/from_sovereign/events.rs
@@ -6,7 +6,7 @@ multiversx_sc::imports!();
 
 #[multiversx_sc::module]
 pub trait EventsModule {
-    #[event("executeBridgeOp")]
+    #[event("executedBridgeOp")]
     fn execute_bridge_operation_event(
         &self,
         #[indexed] hash_of_hashes: ManagedBuffer,
