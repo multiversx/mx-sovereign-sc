435cb21f756c1016918c1a0a69c3ed4add1cc6ef
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-03-17 13:08:55 +0200

    Modified blackbox esdt safe setup

--- esdt-safe/tests/esdt_safe_blackbox_test.rs
+++ esdt-safe/tests/esdt_safe_blackbox_test.rs
@@ -1,13 +1,13 @@
-use multiversx_sc::{
-    api::StaticVarApi,
-    types::{Address, ManagedAddress, ManagedBuffer},
-};
+use multiversx_sc::
+    types::{Address, ManagedByteArray}
+;
 use multiversx_sc_scenario::{
-    api::StaticApi, scenario_model::{AddressValue, SetStateStep}, ContractInfo, ScenarioWorld,
+    api::StaticApi, scenario_model::{Account, AddressValue, SetStateStep}, ContractInfo, ScenarioWorld,
 };
 
 const ESDT_SAFE_PATH_EXPR: &str = "file:output/esdt_safe.wasm";
 const INITIATOR_ADDRESS_EXPR: &str = "address:owner";
+const SIGNERS_COUNT: u32 = 10;
 
 type ESDTSafeContract = ContractInfo<esdt_safe::Proxy<StaticApi>>;
 
@@ -21,11 +21,11 @@ fn world() -> ScenarioWorld {
     blockchain
 }
 
-struct ESDTSafeTestState<M: StaticVarApi> {
+struct ESDTSafeTestState {
     world: ScenarioWorld,
     min_valid_signers: u32,
     initiator_address: Address,
-    signers: [Address],
+    signers: [u8],
 }
 
 impl ESDTSafeTestState {
@@ -35,11 +35,11 @@ impl ESDTSafeTestState {
         world.set_state_step(
             SetStateStep::new()
                 .put_account(INITIATOR_ADDRESS_EXPR, Account::new().nonce(1))
-                .put_account(),
         );
 
         let initiator_address = AddressValue::from(INITIATOR_ADDRESS_EXPR).to_address();
-        let min_valid_signers = 10;
+        let min_valid_signers = SIGNERS_COUNT;
+        let signers = ManagedByteArray::new_from_bytes(&[u8, 32]);
 
         Self {
             world,
