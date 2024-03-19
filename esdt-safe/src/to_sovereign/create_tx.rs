98ce5f32b7993528f77de4f97e73302aa699064c
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-03-19 10:37:14 +0200

    Modified deposit logic related to is_sovereign_chain

--- esdt-safe/src/to_sovereign/create_tx.rs
+++ esdt-safe/src/to_sovereign/create_tx.rs
@@ -204,15 +204,53 @@ pub trait CreateTxModule:
 
             current_token_data.amount = payment.amount.clone();
 
-            let mx_token_id_state = self
-                .sovereign_to_multiversx_token_id(&payment.token_identifier)
-                .get();
+            if self.is_sovereign_chain_mapper().get() {
+                if payment.token_nonce == 0 {
+                    self.send()
+                        .esdt_local_burn(&payment.token_identifier, 0, &payment.amount);
 
-            match mx_token_id_state {
-                TokenMapperState::Token(mx_token_id) => {
+                    event_payments.push(MultiValue3((
+                        payment.token_identifier,
+                        0,
+                        current_token_data.clone(),
+                    )));
+
+                    continue;
+                }
+
+                let multiversx_esdt_token_info = self
+                    .multiversx_esdt_token_info_mapper(
+                        &payment.token_identifier,
+                        &payment.token_nonce,
+                    )
+                    .get();
+
+                self.send().esdt_local_burn(
+                    &multiversx_esdt_token_info.token_identifier,
+                    multiversx_esdt_token_info.token_nonce,
+                    &payment.amount,
+                );
+
+                self.sovereign_esdt_token_info_mapper(
+                    &payment.token_identifier,
+                    &payment.token_nonce,
+                )
+                .take();
+
+                event_payments.push(MultiValue3((
+                    payment.token_identifier.clone(),
+                    multiversx_esdt_token_info.token_nonce,
+                    current_token_data.clone(),
+                )));
+            } else {
+                let sov_token_id = self
+                    .multiversx_to_sovereign_token_id(&payment.token_identifier)
+                    .get();
+
+                if sov_token_id.is_valid_esdt_identifier() {
                     if payment.token_nonce == 0 {
                         self.send()
-                            .esdt_local_burn(&mx_token_id, 0, &payment.amount);
+                            .esdt_local_burn(&sov_token_id, 0, &payment.amount);
 
                         event_payments.push(MultiValue3((
                             payment.token_identifier,
@@ -223,16 +261,16 @@ pub trait CreateTxModule:
                         continue;
                     }
 
-                    let sovereign_esdt_token_info = self
-                        .sovereign_esdt_token_info_mapper(
+                    let multiversx_esdt_token_info = self
+                        .multiversx_esdt_token_info_mapper(
                             &payment.token_identifier,
                             &payment.token_nonce,
                         )
                         .get();
 
                     self.send().esdt_local_burn(
-                        &sovereign_esdt_token_info.token_identifier,
-                        sovereign_esdt_token_info.token_nonce,
+                        &multiversx_esdt_token_info.token_identifier,
+                        multiversx_esdt_token_info.token_nonce,
                         &payment.amount,
                     );
 
@@ -243,22 +281,19 @@ pub trait CreateTxModule:
                     .take();
 
                     event_payments.push(MultiValue3((
-                        mx_token_id.clone(),
-                        sovereign_esdt_token_info.token_nonce,
-                        current_token_data,
+                        sov_token_id.clone(),
+                        multiversx_esdt_token_info.token_nonce,
+                        current_token_data.clone(),
+                    )));
+                } else {
+                    event_payments.push(MultiValue3((
+                        sov_token_id.clone(),
+                        payment.token_nonce,
+                        current_token_data.clone(),
                     )));
                 }
-                _ => {
-                    let sov_token_id = self
-                        .multiversx_to_sovereign_token_id(&payment.token_identifier)
-                        .get();
-
-                    if sov_token_id.is_valid_esdt_identifier() {
-
-                    }
-                }
             }
-        };
+        }
 
         let caller = self.blockchain().get_caller();
 
