bb67d8cbb8c132e0f43c10cc7b0137d8d0e2e230
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-04-04 16:18:19 +0300

    Commented bridge blackbox test

--- esdt-safe/tests/esdt_safe_blackbox_test.rs
+++ esdt-safe/tests/esdt_safe_blackbox_test.rs
@@ -1,32 +1,33 @@
-use multiversx_sc::
-    types::{Address, ManagedByteArray}
-;
-use multiversx_sc_scenario::{
-    api::StaticApi, scenario_model::{Account, AddressValue, SetStateStep}, ContractInfo, ScenarioWorld,
-};
-
-const ESDT_SAFE_PATH_EXPR: &str = "file:output/esdt_safe.wasm";
-const INITIATOR_ADDRESS_EXPR: &str = "address:owner";
-const SIGNERS_COUNT: u32 = 10;
-
-type ESDTSafeContract = ContractInfo<esdt_safe::Proxy<StaticApi>>;
-
-fn world() -> ScenarioWorld {
-    let mut blockchain = ScenarioWorld::new();
-
-    blockchain.set_current_dir_from_workspace("esdt-safe/src/lib");
-
-    blockchain.register_contract(ESDT_SAFE_PATH_EXPR, esdt_safe::ContractBuilder);
-
-    blockchain
-}
-
-struct ESDTSafeTestState {
-    world: ScenarioWorld,
-    min_valid_signers: u32,
-    initiator_address: Address,
-    signers: [u8],
-}
+// use multiversx_sc::
+//     types::Address
+// ;
+// use multiversx_sc_scenario::{
+//     ContractInfo, ScenarioWorld,
+// };
+// use multiversx_sc_scenario::api::StaticApi;
+//
+// const ESDT_SAFE_PATH_EXPR: &str = "file:output/esdt_safe.wasm";
+// const INITIATOR_ADDRESS_EXPR: &str = "address:owner";
+// const SIGNERS_COUNT: u32 = 10;
+//
+// type ESDTSafeContract = ContractInfo<esdt_safe::Proxy<StaticApi>>;
+//
+// fn world() -> ScenarioWorld {
+//     let mut blockchain = ScenarioWorld::new();
+//
+//     blockchain.set_current_dir_from_workspace("esdt-safe/src/lib");
+//
+//     blockchain.register_contract(ESDT_SAFE_PATH_EXPR, esdt_safe::ContractBuilder);
+//
+//     blockchain
+// }
+//
+// struct ESDTSafeTestState {
+//     world: ScenarioWorld,
+//     min_valid_signers: u32,
+//     initiator_address: Address,
+//     signers: [u8],
+// }
 
 // impl ESDTSafeTestState {
 //     fn new() -> Self {
