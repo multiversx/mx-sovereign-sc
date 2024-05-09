e16fac6b7f92fa17f10b15d1bc549f1ae72c7d6b
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-05-09 09:41:16 +0300

    Started unified syntax setup

--- esdt-safe/tests/bridge_blackbox_tests.rs
+++ esdt-safe/tests/bridge_blackbox_tests.rs
@@ -1,23 +1,27 @@
-use esdt_safe::{to_sovereign::create_tx::ProxyTrait, ProxyTrait as _};
 use multiversx_sc::{
     imports::{MultiValue3, OptionalValue},
-    types::{Address, ManagedAddress, ManagedBuffer, ManagedVec, MultiValueEncoded},
+    types::{
+        Address, ManagedAddress, ManagedBuffer, ManagedVec, MultiValueEncoded, TestAddress,
+        TestSCAddress,
+    },
 };
 use multiversx_sc_scenario::{
     api::StaticApi,
+    imports::MxscPath,
     managed_address, rust_biguint,
     scenario_model::{Account, AddressValue, ScCallStep, ScDeployStep, SetStateStep, TxExpect},
     ContractInfo, ScenarioWorld,
 };
+use esdt_safe::es
 use transaction::GasLimit;
 
-const BRIDGE_PATH_EXPR: &str = "mxsc:output/esdt-safe.mxsc.json";
-const BRIDGE_ADDRESS_EXPR: &str = "sc:bridge";
-const OWNER_ADDRESS_EXPR: &str = "address:owner";
-const OWNER_BALANCE_EXPR: &str = "100,000,000";
-const RECEIVER_ADDRESS_EXPR: &str = "address:receiver";
-const FIRST_SINGER_ADDRESS_EXPR: &str = "address:first_signer";
-const SECOND_SINGER_ADDRESS_EXPR: &str = "address:second_signer";
+const BRIDGE_ADDRESS: TestSCAddress = TestSCAddress::new("bridge");
+const BRIDGE_CODE_PATH: MxscPath = MxscPath::new("output/esdt-safe.mxsc.json");
+// const HEADER_VERIFIER_ADDRESS: TestSCAddress = TestSCAddress::new("header_verifier");
+const OWNER_ADDRESS: TestAddress = TestAddress::new("owner");
+const USER_ADDRESS: TestAddress = TestAddress::new("user");
+const OWNER_BALANCE: u64 = 100_000_000;
+const USER_BALANCE: u64 = 100_000_000;
 
 type BridgeContract = ContractInfo<esdt_safe::Proxy<StaticApi>>;
 
@@ -25,69 +29,87 @@ fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
 
     blockchain.set_current_dir_from_workspace("esdt-safe");
-    blockchain.register_contract(BRIDGE_PATH_EXPR, esdt_safe::ContractBuilder);
+    blockchain.register_contract(BRIDGE_CODE_PATH, esdt_safe::ContractBuilder);
 
     blockchain
 }
 
 struct BridgeTestState {
     world: ScenarioWorld,
-    bridge_contract: BridgeContract,
-    is_sovereign_chain: bool,
-    min_valid_signers: u32,
-    initiator_address: Address,
-    signers: MultiValueEncoded<StaticApi, ManagedAddress<StaticApi>>,
+    // bridge_contract: BridgeContract,
+    // is_sovereign_chain: bool,
+    // min_valid_signers: u32,
+    // initiator_address: Address,
+    // signers: MultiValueEncoded<StaticApi, ManagedAddress<StaticApi>>,
 }
 
 impl BridgeTestState {
     fn new(is_sovereign_chain: bool) -> Self {
         let mut world = world();
 
-        let initiator_address = AddressValue::from(OWNER_ADDRESS_EXPR).to_address();
-        let first_signer_account = AddressValue::from(FIRST_SINGER_ADDRESS_EXPR).to_address();
-        let second_signer_account = AddressValue::from(SECOND_SINGER_ADDRESS_EXPR).to_address();
-        let mut signers: MultiValueEncoded<_, multiversx_sc::types::ManagedAddress<_>> =
-            MultiValueEncoded::new();
+        world
+            .account(OWNER_ADDRESS)
+            .nonce(1)
+            .balance(OWNER_BALANCE)
+            .account(USER_ADDRESS)
+            .nonce(1)
+            .balance(USER_BALANCE);
 
-        signers.push(managed_address!(&first_signer_account));
-        signers.push(managed_address!(&second_signer_account));
-
-        world.set_state_step(
-            SetStateStep::new()
-                .put_account(
-                    OWNER_ADDRESS_EXPR,
-                    Account::new().nonce(1).balance(OWNER_BALANCE_EXPR),
-                )
-                .new_address(OWNER_ADDRESS_EXPR, 1, BRIDGE_ADDRESS_EXPR),
-        );
-
-        let bridge_contract = BridgeContract::new(BRIDGE_ADDRESS_EXPR);
-
-        Self {
-            world,
-            bridge_contract,
-            is_sovereign_chain,
-            min_valid_signers: 2,
-            initiator_address,
-            signers,
-        }
+        Self { world: world }
     }
+    // fn new(is_sovereign_chain: bool) -> Self {
+    //     let mut world = world();
+    //
+    //     let initiator_address = AddressValue::from(OWNER_ADDRESS_EXPR).to_address();
+    //     let first_signer_account = AddressValue::from(FIRST_SINGER_ADDRESS_EXPR).to_address();
+    //     let second_signer_account = AddressValue::from(SECOND_SINGER_ADDRESS_EXPR).to_address();
+    //     let mut signers: MultiValueEncoded<_, multiversx_sc::types::ManagedAddress<_>> =
+    //         MultiValueEncoded::new();
+    //
+    //     signers.push(managed_address!(&first_signer_account));
+    //     signers.push(managed_address!(&second_signer_account));
+    //
+    //     world.set_state_step(
+    //         SetStateStep::new()
+    //             .put_account(
+    //                 OWNER_ADDRESS_EXPR,
+    //                 Account::new().nonce(1).balance(OWNER_BALANCE_EXPR),
+    //             )
+    //             .new_address(OWNER_ADDRESS_EXPR, 1, BRIDGE_ADDRESS_EXPR),
+    //     );
+    //
+    //     let bridge_contract = BridgeContract::new(BRIDGE_ADDRESS_EXPR);
+    //
+    //     Self {
+    //         world,
+    //         bridge_contract,
+    //         is_sovereign_chain,
+    //         min_valid_signers: 2,
+    //         initiator_address,
+    //         signers,
+    //     }
+    // }
 
     fn deploy_bridge_contract(&mut self) -> &mut Self {
-        let bridge_code = self.world.code_expression(BRIDGE_PATH_EXPR);
-
-        self.world.sc_deploy(
-            ScDeployStep::new()
-                .from(OWNER_ADDRESS_EXPR)
-                .code(bridge_code.clone())
-                .call(self.bridge_contract.init(
-                    self.is_sovereign_chain,
-                    self.min_valid_signers,
-                    self.initiator_address.clone(),
-                    self.signers.clone(),
-                ))
-                .expect(TxExpect::ok()),
-        );
+        self.world
+            .tx()
+            .from(OWNER_ADDRESS)
+            .typed(
+            .init()
+            .code(BRIDGE_CODE_PATH)
+            .new_address(BRIDGE_ADDRESS);
+        // self.world.sc_deploy(
+        //     ScDeployStep::new()
+        //         .from(OWNER_ADDRESS_EXPR)
+        //         .code(bridge_code.clone())
+        //         .call(self.bridge_contract.init(
+        //             self.is_sovereign_chain,
+        //             self.min_valid_signers,
+        //             self.initiator_address.clone(),
+        //             self.signers.clone(),
+        //         ))
+        //         .expect(TxExpect::ok()),
+        // );
 
         // self.world.sc_call(
         //     ScCallStep::new()
@@ -110,13 +132,13 @@ impl BridgeTestState {
             >,
         >,
     ) {
-        self.world.sc_call_get_result(
-            ScCallStep::new()
-                .from(OWNER_ADDRESS_EXPR)
-                .egld_value(rust_biguint!(1))
-                .call(self.bridge_contract.deposit(to, opt_transfer_data))
-                .expect(TxExpect::ok()),
-        )
+        // self.world.sc_call_get_result(
+        //     ScCallStep::new()
+        //         .from(OWNER_ADDRESS_EXPR)
+        //         .egld_value(rust_biguint!(1))
+        //         .call(self.bridge_contract.deposit(to, opt_transfer_data))
+        //         .expect(TxExpect::ok()),
+        // )
     }
 }
 
