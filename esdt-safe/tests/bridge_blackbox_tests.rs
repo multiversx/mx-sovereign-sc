bf9c97fd1e131d84c472f71b734d6b8b53742070
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-05-10 16:42:18 +0300

    Added more setup

--- esdt-safe/tests/bridge_blackbox_tests.rs
+++ esdt-safe/tests/bridge_blackbox_tests.rs
@@ -1,21 +1,35 @@
-use esdt_safe::{endpoints::unpause_endpoint, esdt_safe_proxy};
+use esdt_safe::esdt_safe_proxy::{self, EsdtSafeProxy};
+use fee_market::fee_market_proxy;
+use multiversx_sc::types::TestTokenIdentifier;
 use multiversx_sc::{
     imports::{MultiValue3, MultiValueVec, OptionalValue},
-    types::{ManagedBuffer, ManagedVec, ReturnsResult, TestAddress, TestSCAddress, Tx},
+    types::{
+        BigUint, EsdtTokenPayment, ManagedBuffer, ManagedVec, ReturnsResult, TestAddress,
+        TestSCAddress,
+    },
 };
 use multiversx_sc_scenario::{
-    api::StaticApi, imports::MxscPath, ExpectError, ScenarioTxEnv, ScenarioTxRun, ScenarioWorld
+    api::StaticApi, imports::MxscPath, ExpectError, ScenarioTxRun, ScenarioWorld,
 };
 
 const BRIDGE_ADDRESS: TestSCAddress = TestSCAddress::new("bridge");
 const BRIDGE_CODE_PATH: MxscPath = MxscPath::new("output/esdt-safe.mxsc.json");
 const BRIDGE_OWNER_ADDRESS: TestAddress = TestAddress::new("bridge_owner");
 
+const FEE_MARKET_ADDRESS: TestSCAddress = TestSCAddress::new("fee_market");
+const FEE_MARKET_CODE_PATH: MxscPath = MxscPath::new("../fee-market/output/fee-market.mxsc.json");
+
+const PRICE_AGGREGATOR_ADDRESS: TestSCAddress = TestSCAddress::new("price_aggregator");
+
 const USER_ADDRESS: TestAddress = TestAddress::new("user");
 const RECEIVER_ADDRESS: TestAddress = TestAddress::new("receiver");
 
 const BRIDGE_OWNER_BALANCE: u64 = 100_000_000;
 const USER_BALANCE: u64 = 100_000_000;
+
+const NFT_TOKEN_ID: TestTokenIdentifier = TestTokenIdentifier::new("NFT-123456");
+
+const ESDT_PROXY: EsdtSafeProxy = esdt_safe_proxy::EsdtSafeProxy;
 
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
@@ -39,6 +53,7 @@ impl BridgeTestState {
             .nonce(1)
             .balance(BRIDGE_OWNER_BALANCE)
             .account(USER_ADDRESS)
+            .esdt_nft_balance(NFT_TOKEN_ID, 1, 100, ManagedBuffer::new())
             .nonce(1)
             .balance(USER_BALANCE)
             .account(RECEIVER_ADDRESS)
@@ -53,10 +68,35 @@ impl BridgeTestState {
         self.world
             .tx()
             .from(BRIDGE_OWNER_ADDRESS)
-            .typed(esdt_safe_proxy::EsdtSafeProxy)
+            .typed(ESDT_PROXY)
             .init(is_sovereign_chain, 1u32, BRIDGE_OWNER_ADDRESS, signers)
             .code(BRIDGE_CODE_PATH)
             .new_address(BRIDGE_ADDRESS)
+            .run();
+
+        // self.deploy_fee_market_contract();
+
+        self.propose_set_unpaused();
+
+        self
+    }
+
+    fn deploy_fee_market_contract(&mut self) -> &mut Self {
+        let usdc_token_id = TestTokenIdentifier::new("USDC");
+        let wegld_token_id = TestTokenIdentifier::new("WEGLD");
+
+        self.world
+            .tx()
+            .from(BRIDGE_OWNER_ADDRESS)
+            .typed(fee_market_proxy::FeeMarketProxy)
+            .init(
+                BRIDGE_ADDRESS,
+                PRICE_AGGREGATOR_ADDRESS,
+                usdc_token_id,
+                wegld_token_id,
+            )
+            .code(FEE_MARKET_CODE_PATH)
+            .new_address(FEE_MARKET_ADDRESS)
             .run();
 
         self
@@ -75,23 +115,60 @@ impl BridgeTestState {
             .tx()
             .from(USER_ADDRESS)
             .to(BRIDGE_ADDRESS)
-            .typed(esdt_safe_proxy::EsdtSafeProxy)
+            .typed(ESDT_PROXY)
             .deposit(RECEIVER_ADDRESS, transfer_data)
             .egld(10)
             .with_result(ExpectError(4, err_message))
             .run();
     }
 
+    fn propose_nft_deposit(&mut self) {
+        let transfer_data = OptionalValue::<
+            MultiValue3<
+                u64,
+                ManagedBuffer<StaticApi>,
+                ManagedVec<StaticApi, ManagedBuffer<StaticApi>>,
+            >,
+        >::None;
+
+        let payment = EsdtTokenPayment::new(NFT_TOKEN_ID.into(), 1, BigUint::from(10u64));
+
+        self.world
+            .tx()
+            .from(USER_ADDRESS)
+            .to(BRIDGE_ADDRESS)
+            .typed(ESDT_PROXY)
+            .deposit(RECEIVER_ADDRESS, transfer_data)
+            .single_esdt(
+                &payment.token_identifier,
+                payment.token_nonce,
+                &payment.amount,
+            )
+            .with_result(ReturnsResult)
+            .run();
+    }
+
     fn propose_set_unpaused(&mut self) {
-        self
-            .world
+        self.world
             .tx()
             .from(BRIDGE_OWNER_ADDRESS)
             .to(BRIDGE_ADDRESS)
-            .typed(esdt_safe_proxy::EsdtSafeProxy)
+            .typed(ESDT_PROXY)
             .unpause_endpoint()
             .returns(ReturnsResult)
             .run();
+    }
+
+    // fn propose_set_fee_market_address(&mut self) {
+    //     self.world
+    // }
+
+    fn propose_execute_operations(&mut self) {
+        self.world
+            .tx()
+            .from(BRIDGE_OWNER_ADDRESS)
+            .to(BRIDGE_ADDRESS)
+            .typed(ESDT_PROXY);
     }
 }
 
@@ -108,6 +185,15 @@ fn test_egld_deposit_nothing_to_transfer() {
     let err_message = "Nothing to transfer";
 
     state.deploy_bridge_contract(false);
-    state.propose_set_unpaused();
+
     state.propose_egld_deposit_and_expect_err(err_message);
+}
+
+#[test]
+fn test_esdt_deposit() {
+    let mut state = BridgeTestState::new();
+
+    state.deploy_bridge_contract(false);
+
+    state.propose_nft_deposit();
 }
