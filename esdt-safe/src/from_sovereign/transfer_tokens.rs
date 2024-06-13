2394c54a1531a64e5e0cef18d8aaf0127bff3e40
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-06-13 11:42:54 +0300

    Modified transaction call

--- esdt-safe/src/from_sovereign/transfer_tokens.rs
+++ esdt-safe/src/from_sovereign/transfer_tokens.rs
@@ -222,11 +222,12 @@ pub trait TransferTokensModule:
         }
 
         let header_verifier_address = self.header_verifier_address().get();
-        let _ = self
-            .tx()
+
+        self.tx()
             .to(header_verifier_address)
             .typed(header_verifier_proxy::HeaderverifierProxy)
-            .remove_executed_hash(hash_of_hashes, &operation_tuple.op_hash);
+            .remove_executed_hash(hash_of_hashes, &operation_tuple.op_hash)
+            .async_call_and_exit();
     }
 
     fn emit_transfer_failed_events(
