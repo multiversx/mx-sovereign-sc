b6a8743fd81e6e741d1cf83199f3966c53429035
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-07-04 14:20:03 +0300

    Modified inline function and used new has_sov_prefix function

--- enshrine-esdt-safe/src/from_sovereign/transfer_tokens.rs
+++ enshrine-esdt-safe/src/from_sovereign/transfer_tokens.rs
@@ -183,7 +183,10 @@ pub trait TransferTokensModule:
         let mut output_payments = ManagedVec::new();
 
         for operation_token in operation_tokens.iter() {
-            if !self.has_prefix(&operation_token.token_identifier) {
+            if !self.has_sov_prefix(
+                &operation_token.token_identifier,
+                self.get_sovereign_prefix(),
+            ) {
                 output_payments.push(operation_token.clone());
                 continue;
             }
@@ -341,7 +344,7 @@ pub trait TransferTokensModule:
 
     fn burn_sovereign_tokens(&self, operation: &Operation<Self::Api>) {
         for token in operation.tokens.iter() {
-            if self.has_prefix(&token.token_identifier) {
+            if self.has_sov_prefix(&token.token_identifier, self.get_sovereign_prefix()) {
                 self.send().esdt_local_burn(
                     &token.token_identifier,
                     token.token_nonce,
@@ -405,7 +408,13 @@ pub trait TransferTokensModule:
 
     #[inline]
     fn was_token_registered(&self, token_id: &TokenIdentifier<Self::Api>) -> bool {
-        self.has_prefix(token_id) && self.paid_issued_tokens().contains(token_id)
+        self.has_sov_prefix(token_id, self.get_sovereign_prefix())
+            && self.paid_issued_tokens().contains(token_id)
+    }
+
+    #[inline]
+    fn get_sovereign_prefix(&self) -> ManagedBuffer {
+        self.sovereign_token_prefix().get()
     }
 
     #[inline]
