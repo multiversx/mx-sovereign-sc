ea43883da81f27f51460eff727ce1dedd4f42ea7
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-08-02 14:01:57 +0300

    Removed storage.rs file
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- token-handler/src/lib.rs
+++ token-handler/src/lib.rs
@@ -3,16 +3,12 @@
 #[allow(unused_imports)]
 use multiversx_sc::imports::*;
 
-pub mod storage;
 pub mod token_handler_proxy;
 pub mod transfer_tokens;
 
 #[multiversx_sc::contract]
 pub trait TokenHandler:
-    transfer_tokens::TransferTokensModule
-    + utils::UtilsModule
-    + storage::CommonStorage
-    + tx_batch_module::TxBatchModule
+    transfer_tokens::TransferTokensModule + utils::UtilsModule + tx_batch_module::TxBatchModule
 {
     #[init]
     fn init(&self) {}
