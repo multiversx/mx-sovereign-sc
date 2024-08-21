163266b24a9dc224eba4a6c802fbf6b1d64a6dd8
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-08-21 12:16:43 +0300

    Fixed test and added CamelCase for storage mappers
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- esdt-safe/tests/bridge_blackbox_tests.rs
+++ esdt-safe/tests/bridge_blackbox_tests.rs
@@ -270,7 +270,6 @@ impl BridgeTestState {
             .to(BRIDGE_ADDRESS)
             .typed(esdt_safe_proxy::EsdtSafeProxy)
             .execute_operations(hash_of_hashes, operation)
-            .returns(ExpectError(4, "Operation is not registered"))
             .run();
     }
 
