adedd186850bfaf18535d164cfd18262bb3443dd
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-08-27 22:33:08 +0300

    Added chain_id to other endpoints
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- chain-factory/src/slash.rs
+++ chain-factory/src/slash.rs
@@ -20,9 +20,9 @@ mod validators_contract_proxy {
 #[multiversx_sc::module]
 pub trait SlashModule: crate::factory::FactoryModule {
     #[endpoint]
-    fn slash(&self, validator_address: ManagedAddress, value: BigUint) {
+    fn slash(&self, chain_id: ManagedBuffer, validator_address: ManagedAddress, value: BigUint) {
         let caller = self.blockchain().get_caller();
-        self.require_deployed_sc(&caller);
+        self.require_deployed_sc(chain_id, &caller);
 
         let validators_contract_address = self.validators_contract_address().get();
         let _: IgnoreValue = self
@@ -32,9 +32,13 @@ pub trait SlashModule: crate::factory::FactoryModule {
     }
 
     #[endpoint(distributeSlashed)]
-    fn distribute_slashed(&self, dest_amount_pairs: DestAmountPairs<Self::Api>) {
+    fn distribute_slashed(
+        &self,
+        chain_id: ManagedBuffer,
+        dest_amount_pairs: DestAmountPairs<Self::Api>,
+    ) {
         let caller = self.blockchain().get_caller();
-        self.require_deployed_sc(&caller);
+        self.require_deployed_sc(chain_id, &caller);
 
         let validators_contract_address = self.validators_contract_address().get();
         let _: IgnoreValue = self
@@ -43,9 +47,9 @@ pub trait SlashModule: crate::factory::FactoryModule {
             .execute_on_dest_context();
     }
 
-    fn require_deployed_sc(&self, address: &ManagedAddress) {
+    fn require_deployed_sc(&self, chain_id: ManagedBuffer, address: &ManagedAddress) {
         require!(
-            self.all_deployed_contracts().contains(address),
+            self.all_deployed_contracts(chain_id).contains(address),
             "Only deployed contracts may call this endpoint"
         );
     }
