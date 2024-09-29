54a0df09b1794f59cbb2cd08af452db58be98388
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-09-29 16:44:57 +0300

    Updated test setup for header verifier
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- header-verifier/tests/header_verifier_blackbox_test.rs
+++ header-verifier/tests/header_verifier_blackbox_test.rs
@@ -1,125 +1,74 @@
-#![allow(deprecated)]
-
 use bls_signature::BlsSignature;
-use header_verifier::ProxyTrait;
-use multiversx_sc::types::{ManagedBuffer, ManagedByteArray, MultiValueEncoded};
+use header_verifier::header_verifier_proxy;
+use multiversx_sc::types::{
+    BigUint, ManagedBuffer, ManagedByteArray, ManagedVec, MultiValueEncoded, TestAddress,
+    TestSCAddress,
+};
 use multiversx_sc_scenario::{
     api::StaticApi,
-    scenario_model::{Account, ScCallStep, ScDeployStep, SetStateStep, TxExpect},
-    ContractInfo, ScenarioWorld,
+    imports::MxscPath,
+    scenario_model::{ScCallStep, ScDeployStep, TxExpect},
+    ContractInfo, ScenarioTxRun, ScenarioWorld,
 };
 
-const HEADER_VERIFIER_PATH_EXPR: &str = "file:output/header-verifier.wasm";
-const OWNER_ADDRESS_EXPR: &str = "address:owner";
-const LEADER_ADDRESS_EXPR: &str = "address:proposer";
-const VALIDATOR_ADDRESS_EXPR: &str = "address:board-member";
-const HEADER_VERIFIER_ADDRESS_EXPR: &str = "sc:header_verifier";
+const HEADER_VERIFIER_CODE_PATH: MxscPath = MxscPath::new("ouput/header-verifier.mxsc-json");
+const HEADER_VERIFIER_ADDRESS: TestSCAddress = TestSCAddress::new("header-verifier");
+const OWNER: TestAddress = TestAddress::new("owner");
+const LEADER: TestAddress = TestAddress::new("leader");
+const VALIDATOR: TestAddress = TestAddress::new("validator");
+const WEGLD_BALANCE: u128 = 100_000_000_000_000_000;
 
 type HeaderVerifierContract = ContractInfo<header_verifier::Proxy<StaticApi>>;
+type BlsKeys = MultiValueEncoded<StaticApi, ManagedBuffer<StaticApi>>;
 
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
-    blockchain.set_current_dir_from_workspace("header-verifier/src/lib");
-
-    blockchain.register_contract(HEADER_VERIFIER_PATH_EXPR, header_verifier::ContractBuilder);
+    blockchain.register_contract(HEADER_VERIFIER_CODE_PATH, header_verifier::ContractBuilder);
 
     blockchain
 }
 
 struct HeaderVerifierTestState {
     world: ScenarioWorld,
-    header_verifier_contract: HeaderVerifierContract,
 }
 
 impl HeaderVerifierTestState {
     fn new() -> Self {
         let mut world = world();
 
-        world.set_state_step(
-            SetStateStep::new()
-                .put_account(OWNER_ADDRESS_EXPR, Account::new().nonce(1))
-                .new_address(OWNER_ADDRESS_EXPR, 1, HEADER_VERIFIER_ADDRESS_EXPR)
-                .put_account(
-                    LEADER_ADDRESS_EXPR,
-                    Account::new().nonce(1).balance(LEADER_ADDRESS_EXPR),
-                )
-                .put_account(VALIDATOR_ADDRESS_EXPR, Account::new().nonce(1)),
-        );
+        world
+            .account(OWNER)
+            .balance(BigUint::from(WEGLD_BALANCE))
+            .nonce(1);
 
-        let header_verifier_contract = HeaderVerifierContract::new(HEADER_VERIFIER_ADDRESS_EXPR);
-
-        Self {
-            world,
-            header_verifier_contract,
-        }
+        Self { world }
     }
 
-    fn deploy_header_verifier_contract(&mut self) -> &mut Self {
-        let header_verifier_code = self.world.code_expression(HEADER_VERIFIER_PATH_EXPR);
-
-        self.world.sc_deploy(
-            ScDeployStep::new()
-                .from(OWNER_ADDRESS_EXPR)
-                .code(header_verifier_code),
-        );
+    fn deploy_header_verifier_contract(&mut self, bls_keys: BlsKeys) -> &mut Self {
+        self.world
+            .tx()
+            .from(OWNER)
+            .typed(header_verifier_proxy::HeaderverifierProxy)
+            .init(bls_keys)
+            .code(HEADER_VERIFIER_CODE_PATH)
+            .new_address(HEADER_VERIFIER_ADDRESS)
+            .run();
 
         self
     }
 
-    fn _propose_register_bridge_ops(
-        &mut self,
-        bridge_operations_hash: &ManagedBuffer<StaticApi>,
-        operations_hashes: MultiValueEncoded<StaticApi, ManagedBuffer<StaticApi>>,
-        signature: &BlsSignature<StaticApi>,
-    ) {
-        self.world
-            .sc_call_get_result(ScCallStep::new().from(LEADER_ADDRESS_EXPR).call(
-                self.header_verifier_contract.register_bridge_operations(
-                    signature,
-                    bridge_operations_hash,
-                    operations_hashes,
-                ),
-            ))
+    fn get_bls_keys(&mut self, bls_keys_vec: Vec<ManagedBuffer<StaticApi>>) -> BlsKeys {
+        let bls_keys = bls_keys_vec.iter().map(|key| key.clone().into()).collect();
+
+        bls_keys
     }
 }
 
 #[test]
 fn test_deploy() {
     let mut state = HeaderVerifierTestState::new();
-    state.deploy_header_verifier_contract();
-}
+    let bls_key_1 = ManagedBuffer::from("bls_key_1");
+    let managed_bls_keys = state.get_bls_keys(vec![bls_key_1]);
 
-#[test]
-fn test_register_bridge_ops_wrong_hashes() {
-    let mut state = HeaderVerifierTestState::new();
-    state.deploy_header_verifier_contract();
-
-    let bridge_operations_hash =
-        ManagedBuffer::from("6ee1e00813a74f8293d2c63172c062d38bf780d8811ff63984813a49cd61ff9e");
-    let mock_signature: BlsSignature<StaticApi> = ManagedByteArray::new_from_bytes(
-        b"EIZ2\x05\xf7q\xc7G\x96\x1f\xba0\xe2\xd1\xf5pE\x14\xd7?\xac\xff\x8d\x1a\x0c\x11\x900f5\xfb\xff4\x94\xb8@\xc5^\xc2,exn0\xe3\xf0\n"
-    );
-
-    let first_operation: ManagedBuffer<StaticApi> =
-        ManagedBuffer::from("95cdb166d6e12a8c4a783a48d2e4f647e15fac4e5a115d4483f95881630a5433");
-    let second_operation =
-        ManagedBuffer::from("4851cd6e4a4799ad0d8e8ead37c88d930874302ab11edcc60f608654be14b2ed");
-
-    let mut bridge_operations: MultiValueEncoded<StaticApi, ManagedBuffer<StaticApi>> =
-        MultiValueEncoded::new();
-    bridge_operations.push(first_operation);
-    bridge_operations.push(second_operation);
-
-    state.world.sc_call(
-        ScCallStep::new()
-            .from(OWNER_ADDRESS_EXPR)
-            .call(state.header_verifier_contract.register_bridge_operations(
-                mock_signature,
-                bridge_operations_hash,
-                bridge_operations,
-            ))
-            .expect(TxExpect::user_error(
-                "str:Hash of all operations doesn't match the hash of transfer data",
-            )),
-    );
+    state.deploy_header_verifier_contract(managed_bls_keys);
 }
