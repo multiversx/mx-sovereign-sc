34a78cc07528e2c14d37102f3298034078f9f346
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-10-28 15:01:40 +0200

    Made chain-id 4 characters long
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- chain-factory/src/common/utils.rs
+++ chain-factory/src/common/utils.rs
@@ -51,7 +51,7 @@ pub trait UtilsModule: storage::CommonStorage {
 
     fn generate_chain_id(&self) -> ManagedBuffer {
         loop {
-            let new_chain_id = self.generated_random_2_char_string();
+            let new_chain_id = self.generated_random_4_char_string();
             if !self.chain_ids().contains(&new_chain_id) {
                 self.chain_ids().insert(new_chain_id.clone());
 
@@ -60,12 +60,12 @@ pub trait UtilsModule: storage::CommonStorage {
         }
     }
 
-    fn generated_random_2_char_string(&self) -> ManagedBuffer {
-        let mut byte_array: [u8; 2] = [0; 2];
+    fn generated_random_4_char_string(&self) -> ManagedBuffer {
+        let mut byte_array: [u8; 4] = [0; 4];
         let charset: &[u8] = b"0123456789abcdefghijklmnopqrstuvwxyz";
         let mut rand = RandomnessSource::new();
 
-        (0..2).for_each(|i| {
+        (0..4).for_each(|i| {
             let rand_index = rand.next_u8_in_range(0, charset.len() as u8) as usize;
             byte_array[i] = charset[rand_index];
         });
