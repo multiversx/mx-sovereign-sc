7443a3fcfd89390a60825896519f19689ba7f3f2
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-11-12 16:37:51 +0200

    Added token-handler proxy + exported all the proxies
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/proxies/src/lib.rs
+++ common/proxies/src/lib.rs
@@ -1,11 +1,8 @@
 #![no_std]
-mod chain_config_proxy;
-mod chain_factory_proxy;
-mod enshrine_esdt_safe_proxy;
-mod esdt_safe_proxy;
-mod header_verifier_proxy;
-pub use chain_config_proxy::ChainConfigContractProxy;
-pub use chain_factory_proxy::ChainFactoryContractProxy;
-pub use enshrine_esdt_safe_proxy::EnshrineEsdtSafeProxy;
-pub use esdt_safe_proxy::EsdtSafeProxy;
-pub use header_verifier_proxy::HeaderverifierProxy;
+pub mod chain_config_proxy;
+pub mod chain_factory_proxy;
+pub mod enshrine_esdt_safe_proxy;
+pub mod esdt_safe_proxy;
+pub mod fee_market_proxy;
+pub mod header_verifier_proxy;
+pub mod token_handler_proxy;
