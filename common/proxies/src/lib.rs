77670c9af0f106b0d14ebb72cc4f817eaf17871f
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-11-12 16:23:21 +0200

    Added all proxies inside the common folder
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/proxies/src/lib.rs
+++ common/proxies/src/lib.rs
@@ -1,2 +1,11 @@
+#![no_std]
 mod chain_config_proxy;
+mod chain_factory_proxy;
+mod enshrine_esdt_safe_proxy;
+mod esdt_safe_proxy;
+mod header_verifier_proxy;
 pub use chain_config_proxy::ChainConfigContractProxy;
+pub use chain_factory_proxy::ChainFactoryContractProxy;
+pub use enshrine_esdt_safe_proxy::EnshrineEsdtSafeProxy;
+pub use esdt_safe_proxy::EsdtSafeProxy;
+pub use header_verifier_proxy::HeaderverifierProxy;
