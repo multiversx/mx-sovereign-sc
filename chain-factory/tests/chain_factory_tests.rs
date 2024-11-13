a0e6d1afc907e0240087acfe0e42f4e3334a0874
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-11-13 16:56:54 +0200

    Updated tests
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- chain-factory/tests/chain_factory_tests.rs
+++ chain-factory/tests/chain_factory_tests.rs
@@ -1,23 +1,18 @@
 use std::env::current_dir;
 
 use chain_config::StakeMultiArg;
-use chain_factory::common::storage::CommonStorage;
 use multiversx_sc::types::{
-    BigUint, CodeMetadata, ManagedBuffer, MultiValueEncoded, TestAddress, TestSCAddress,
-    TokenIdentifier,
+    BigUint, CodeMetadata, MultiValueEncoded, TestAddress, TestSCAddress, TokenIdentifier,
 };
 use multiversx_sc_scenario::{
-    api::StaticApi, imports::MxscPath, managed_biguint, ExpectError, ScenarioTxRun,
-    ScenarioTxWhitebox, ScenarioWorld,
+    api::StaticApi, imports::MxscPath, managed_biguint, ExpectError, ScenarioTxRun, ScenarioWorld,
 };
 use proxies::{
-    chain_config_proxy::ChainConfigContractProxy,
-    chain_factory_proxy::{ChainFactoryContractProxy, ContractInfo, ScArray},
+    chain_config_proxy::ChainConfigContractProxy, chain_factory_proxy::ChainFactoryContractProxy,
 };
 
 const FACTORY_ADDRESS: TestSCAddress = TestSCAddress::new("chain-factory");
 const CODE_PATH: MxscPath = MxscPath::new("output/chain-factory.mxsc.json");
-const CHAIN_ID: &str = "CHAIN-ID";
 
 const CONFIG_ADDRESS: TestSCAddress = TestSCAddress::new("chain-config");
 const CONFIG_CODE_PATH: MxscPath = MxscPath::new("../chain-config/output/chain-factory.mxsc.json");
@@ -73,30 +68,13 @@ impl ChainFactoryTestState {
             .from(OWNER)
             .typed(ChainFactoryContractProxy)
             .init(
-                FACTORY_ADDRESS,
                 CONFIG_ADDRESS,
                 FACTORY_ADDRESS,
                 FACTORY_ADDRESS,
                 FACTORY_ADDRESS,
-                FACTORY_ADDRESS,
-                managed_biguint!(DEPLOY_COST),
             )
             .code(CODE_PATH)
             .new_address(FACTORY_ADDRESS)
-            .run();
-    }
-
-    fn propose_add_contracts_to_map(
-        &mut self,
-        chain_id: ManagedBuffer<StaticApi>,
-        contracts_info: MultiValueEncoded<StaticApi, ContractInfo<StaticApi>>,
-    ) {
-        self.world
-            .tx()
-            .from(OWNER)
-            .to(FACTORY_ADDRESS)
-            .typed(ChainFactoryContractProxy)
-            .add_contracts_to_map(chain_id, contracts_info)
             .run();
     }
 
@@ -144,61 +122,6 @@ fn deploy() {
 }
 
 #[test]
-fn add_contracts_to_map() {
-    let additional_stake: StakeMultiArg<StaticApi> =
-        (TokenIdentifier::from("TEST-TOKEN"), BigUint::from(100u64)).into();
-    let mut additional_stake_required = MultiValueEncoded::new();
-    additional_stake_required.push(additional_stake);
-
-    let mut state = ChainFactoryTestState::new(&additional_stake_required);
-    state.deploy_chain_factory();
-
-    let contract_info: ContractInfo<StaticApi> = ContractInfo {
-        id: ScArray::ChainConfig,
-        address: CONFIG_ADDRESS.to_managed_address(),
-    };
-    let mut contracts_map = MultiValueEncoded::new();
-    contracts_map.push(contract_info);
-
-    state.propose_add_contracts_to_map(ManagedBuffer::from(CHAIN_ID), contracts_map);
-
-    state
-        .world
-        .query()
-        .to(FACTORY_ADDRESS)
-        .whitebox(chain_factory::contract_obj, |sc| {
-            assert!(!sc
-                .all_deployed_contracts(&OWNER.to_managed_address())
-                .is_empty());
-        })
-}
-
-#[test]
-fn deploy_chain_config_from_factory_deploy_cost_too_low() {
-    let additional_stake: StakeMultiArg<StaticApi> =
-        (TokenIdentifier::from("TEST-TOKEN"), BigUint::from(100u64)).into();
-    let mut additional_stake_required = MultiValueEncoded::new();
-    additional_stake_required.push(additional_stake);
-
-    let mut state = ChainFactoryTestState::new(&additional_stake_required);
-
-    let min_validators = 1;
-    let max_validators = 4;
-    let min_stake = BigUint::from(100_000u64);
-
-    state.deploy_chain_factory();
-
-    state.propose_deploy_chain_config_from_factory(
-        BigUint::from(100u64),
-        min_validators,
-        max_validators,
-        min_stake,
-        additional_stake_required,
-        Some(ExpectError(4, "Invalid payment amount")),
-    );
-}
-
-#[test]
 fn deploy_chain_config_from_factory() {
     let additional_stake: StakeMultiArg<StaticApi> =
         (TokenIdentifier::from("TEST-TOKEN"), BigUint::from(100u64)).into();
@@ -223,14 +146,4 @@ fn deploy_chain_config_from_factory() {
         additional_stake_required,
         None,
     );
-
-    state
-        .world
-        .query()
-        .to(FACTORY_ADDRESS)
-        .whitebox(chain_factory::contract_obj, |sc| {
-            assert!(!sc
-                .all_deployed_contracts(&OWNER.to_managed_address())
-                .is_empty());
-        })
 }
