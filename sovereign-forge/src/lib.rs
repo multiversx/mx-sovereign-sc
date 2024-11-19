ad49eee773a1f6c56111eb79fb729378f543eb6a
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-11-19 08:43:52 +0200

    Added `deploy-cost`
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- sovereign-forge/src/lib.rs
+++ sovereign-forge/src/lib.rs
@@ -9,7 +9,10 @@ mod storage;
 #[multiversx_sc::contract]
 pub trait SovereignForge: phases::PhasesModule + storage::StorageModule {
     #[init]
-    fn init(&self) {}
+    fn init(&self, deploy_cost: BigUint) {
+        require!(deploy_cost > 0, "The deploy cost can't be a 0 value");
+        self.deploy_cost().set(deploy_cost);
+    }
 
     #[upgrade]
     fn upgrade(&self) {}
--- sovereign-forge/src/storage.rs
+++ sovereign-forge/src/storage.rs
@@ -1,7 +1,11 @@
-use multiversx_sc::imports::UnorderedSetMapper;
+use multiversx_sc::imports::{SingleValueMapper, UnorderedSetMapper};
 
 #[multiversx_sc::module]
 pub trait StorageModule {
+    #[view(getDeployCost)]
+    #[storage_mapper("deployCost")]
+    fn deploy_cost(&self) -> SingleValueMapper<BigUint>;
+
     #[view(getAllChainIds)]
     #[storage_mapper("allChainIds")]
     fn chain_ids(&self) -> UnorderedSetMapper<ManagedBuffer>;
