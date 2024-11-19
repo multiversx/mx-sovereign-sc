0a2e10c0d1ce12d765d548d44165356fa7c2b2f9
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-11-19 17:08:47 +0200

    Modified endpoints to use `shard_id` as argument
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- sovereign-forge/src/lib.rs
+++ sovereign-forge/src/lib.rs
@@ -16,33 +16,26 @@ pub trait SovereignForge:
         self.deploy_cost().set(deploy_cost);
     }
 
+    #[only_owner]
     #[endpoint(registerTokenHandler)]
-    fn register_token_handler(&self, token_handler_address: ManagedAddress) {
+    fn register_token_handler(&self, shard_id: u32, token_handler_address: ManagedAddress) {
         require!(
             self.blockchain().is_smart_contract(&token_handler_address),
             "The given address is not a valid SC address"
         );
 
-        let blockchain_api = self.blockchain();
-        let caller = blockchain_api.get_caller();
-        let caller_shard_id = blockchain_api.get_shard_of_address(&caller);
-
-        self.token_handlers(caller_shard_id)
-            .set(token_handler_address);
+        self.token_handlers(shard_id).set(token_handler_address);
     }
 
+    #[only_owner]
     #[endpoint(registerChainFactory)]
-    fn register_chain_factory(&self, chain_factory_address: ManagedAddress) {
+    fn register_chain_factory(&self, shard_id: u32, chain_factory_address: ManagedAddress) {
         require!(
             self.blockchain().is_smart_contract(&chain_factory_address),
             "The given address is not a valid SC address"
         );
 
-        let caller = self.blockchain().get_caller();
-        let caller_shard_id = self.blockchain().get_shard_of_address(&caller);
-
-        self.token_handlers(caller_shard_id)
-            .set(chain_factory_address);
+        self.token_handlers(shard_id).set(chain_factory_address);
     }
 
     #[upgrade]
