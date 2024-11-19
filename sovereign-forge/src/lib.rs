9630f7938753eab181a3ea74d78c93fcc9a378ab
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-11-19 14:52:33 +0200

    Added endpoints to register token-handler and chain-factory addresses
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- sovereign-forge/src/lib.rs
+++ sovereign-forge/src/lib.rs
@@ -16,6 +16,32 @@ pub trait SovereignForge:
         self.deploy_cost().set(deploy_cost);
     }
 
+    #[endpoint(registerTokenHandler)]
+    fn register_token_handler(&self, token_handler_address: ManagedAddress) {
+        require!(
+            self.blockchain().is_smart_contract(&token_handler_address),
+            "The given address is not a valid SC address"
+        );
+
+        let caller = self.blockchain().get_caller();
+        let caller_shard = self.blockchain().get_shard_of_address(&caller);
+
+        self.token_handlers(caller_shard).set(token_handler_address);
+    }
+
+    #[endpoint(registerChainFactory)]
+    fn register_chain_factory(&self, chain_factory_address: ManagedAddress) {
+        require!(
+            self.blockchain().is_smart_contract(&chain_factory_address),
+            "The given address is not a valid SC address"
+        );
+
+        let caller = self.blockchain().get_caller();
+        let caller_shard = self.blockchain().get_shard_of_address(&caller);
+
+        self.token_handlers(caller_shard).set(chain_factory_address);
+    }
+
     #[upgrade]
     fn upgrade(&self) {}
 }
