88d9205fb9cd7b529362dba3154123f9a5a110c6
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-11-19 15:25:40 +0200

    Added endpoint for chain_factory call
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- sovereign-forge/src/phases.rs
+++ sovereign-forge/src/phases.rs
@@ -1,15 +1,86 @@
 use core::ops::Deref;
+use proxies::chain_factory_proxy::ChainFactoryContractProxy;
+use transaction::StakeMultiArg;
 
-use crate::{common, err_msg};
+use multiversx_sc::{
+    require,
+    types::{ManagedVec, MultiValueEncoded, ReturnsResult},
+};
+
+use crate::{
+    common::{
+        self,
+        utils::{ChainContractsMap, ContractInfo, ScArray},
+    },
+    err_msg,
+};
 
 #[multiversx_sc::module]
 pub trait PhasesModule: common::utils::UtilsModule + common::storage::StorageModule {
     #[payable("EGLD")]
     #[endpoint(deployPhaseOne)]
-    fn deploy_phase_one(&self) {
+    fn deploy_phase_one(
+        &self,
+        min_validators: u64,
+        max_validators: u64,
+        min_stake: BigUint,
+        additional_stake_required: MultiValueEncoded<StakeMultiArg<Self::Api>>,
+    ) {
         let call_value = self.call_value().egld_value();
         self.require_correct_deploy_cost(call_value.deref());
 
+        let blockchain_api = self.blockchain();
+        let caller = blockchain_api.get_caller();
+        let caller_shard_id = blockchain_api.get_shard_of_address(&caller);
+
         let chain_id = self.generate_chain_id();
+        let chain_factory_address = self.chain_factories(caller_shard_id).get();
+
+        let chain_config_address = self.deploy_chain_config(
+            chain_factory_address,
+            min_validators,
+            max_validators,
+            min_stake,
+            additional_stake_required,
+        );
+
+        let sovereigns_mapper = self.sovereigns_mapper(&caller);
+        require!(
+            !sovereigns_mapper
+                .get()
+                .contracts_info
+                .iter()
+                .any(|ci| ci.id == ScArray::ChainConfig),
+            "There is already a registered ChainConfig SC for this chain"
+        );
+
+        let chain_factory_contract_info =
+            ContractInfo::new(ScArray::ChainConfig, chain_config_address);
+        let mut contracts_info = ManagedVec::new();
+        contracts_info.push(chain_factory_contract_info);
+        let chain_contracts_map = ChainContractsMap::new(chain_id, contracts_info);
+
+        sovereigns_mapper.set(chain_contracts_map);
+    }
+
+    fn deploy_chain_config(
+        &self,
+        chain_factory_address: ManagedAddress,
+        min_validators: u64,
+        max_validators: u64,
+        min_stake: BigUint,
+        additional_stake_required: MultiValueEncoded<StakeMultiArg<Self::Api>>,
+    ) -> ManagedAddress {
+        self.tx()
+            .to(chain_factory_address)
+            .typed(ChainFactoryContractProxy)
+            .deploy_sovereign_chain_config_contract(
+                min_validators,
+                max_validators,
+                min_stake,
+                additional_stake_required,
+            )
+            .returns(ReturnsResult)
+            .sync_call()
     }
 }
