d316ac2afb96e1cf9f9d9cdcf0412649e4faf010
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-12-09 16:26:31 +0200

    Added mock addresses for test setup
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- chain-factory/tests/chain_factory_tests.rs
+++ chain-factory/tests/chain_factory_tests.rs
@@ -17,6 +17,10 @@ const CODE_PATH: MxscPath = MxscPath::new("output/chain-factory.mxsc.json");
 const CONFIG_ADDRESS: TestSCAddress = TestSCAddress::new("chain-config");
 const CONFIG_CODE_PATH: MxscPath = MxscPath::new("../chain-config/output/chain-factory.mxsc.json");
 
+const HEADER_ADDRESS: TestSCAddress = TestSCAddress::new("header-verifier");
+const ESDT_SAFE_ADDRESS: TestSCAddress = TestSCAddress::new("esdt-safe");
+const FEE_MARKET_ADDRESS: TestSCAddress = TestSCAddress::new("esdt-safe");
+
 const OWNER: TestAddress = TestAddress::new("owner");
 const OWNER_BALANCE: u64 = 100_000_000_000;
 
@@ -69,9 +73,9 @@ impl ChainFactoryTestState {
             .init(
                 CONFIG_ADDRESS,
                 CONFIG_ADDRESS,
-                FACTORY_ADDRESS,
-                FACTORY_ADDRESS,
-                FACTORY_ADDRESS,
+                HEADER_ADDRESS,
+                ESDT_SAFE_ADDRESS,
+                FEE_MARKET_ADDRESS,
             )
             .code(CODE_PATH)
             .new_address(FACTORY_ADDRESS)
