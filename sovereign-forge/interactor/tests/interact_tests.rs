3646982a0f481888c477966bd92e43a448182faa
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-12-10 11:56:07 +0200

    Uncommented on-chain test
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- sovereign-forge/interactor/tests/interact_tests.rs
+++ sovereign-forge/interactor/tests/interact_tests.rs
@@ -1,19 +1,29 @@
 use forge_rust_interact::ContractInteract;
-use multiversx_sc_snippets::imports::*;
+use multiversx_sc_snippets::imports::tokio;
 
 // Simple deploy test that runs on the real blockchain configuration.
 // In order for this test to work, make sure that the `config.toml` file contains the real blockchain config (or choose it manually)
 // Can be run with `sc-meta test`.
-// #[tokio::test]
-// #[ignore = "run on demand, relies on real blockchain state"]
-// async fn deploy_test_sovereign_forge() {
-//     let mut interactor = ContractInteract::new().await;
-//
-//     interactor.deploy().await;
-//     interactor.deploy_chain_config().await;
-//     interactor.deploy_header_verifier().await;
-//
-//     interactor.deploy_phase_one().await;
-//     interactor.deploy_phase_two().await;
-//     interactor.deploy_phase_three().await;
-// }
+#[tokio::test]
+#[ignore = "run on demand, relies on real blockchain state"]
+async fn deploy_test_sovereign_forge() {
+    let mut interactor = ContractInteract::new().await;
+    interactor.deploy().await;
+
+    interactor.deploy_chain_factory().await;
+    interactor.deploy_chain_config_template().await;
+    interactor.deploy_header_verifier_template().await;
+
+    interactor.register_token_handler(1).await;
+    interactor.register_token_handler(2).await;
+    interactor.register_token_handler(3).await;
+    interactor.register_chain_factory(1).await;
+    interactor.register_chain_factory(2).await;
+    interactor.register_chain_factory(3).await;
+
+    interactor.complete_setup_phase().await;
+
+    interactor.deploy_phase_one().await;
+    interactor.deploy_phase_two().await;
+    interactor.deploy_phase_three().await;
+}
