767794cc2ef8302bf900e147a7182f7cd0590738
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-12-11 14:18:49 +0200

    Removed comments
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- sovereign-forge/interactor/tests/interact_cs_tests.rs
+++ sovereign-forge/interactor/tests/interact_cs_tests.rs
@@ -1,11 +1,6 @@
 use forge_rust_interact::ContractInteract;
 use multiversx_sc_snippets::imports::*;
 
-// Simple deploy test that runs using the chain simulator configuration.
-// In order for this test to work, make sure that the `config.toml` file contains the chain simulator config (or choose it manually)
-// The chain simulator should already be installed and running before attempting to run this test.
-// The chain-simulator-tests feature should be present in Cargo.toml.
-// Can be run with `sc-meta test -c`.
 #[tokio::test]
 #[cfg_attr(not(feature = "chain-simulator-tests"), ignore)]
 async fn deploy_test_sovereign_forge_cs() {
--- sovereign-forge/interactor/tests/interact_tests.rs
+++ sovereign-forge/interactor/tests/interact_tests.rs
@@ -1,9 +1,6 @@
 use forge_rust_interact::ContractInteract;
 use multiversx_sc_snippets::imports::tokio;
 
-// Simple deploy test that runs on the real blockchain configuration.
-// In order for this test to work, make sure that the `config.toml` file contains the real blockchain config (or choose it manually)
-// Can be run with `sc-meta test`.
 #[tokio::test]
 #[ignore = "run on demand, relies on real blockchain state"]
 async fn deploy_test_sovereign_forge() {
