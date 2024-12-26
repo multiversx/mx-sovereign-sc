ef292c9768cea1372ec14125b1ef0a1abca58852
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-12-26 11:21:51 +0200

    Removed unused aliases
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/transaction/src/aliases.rs
+++ common/transaction/src/aliases.rs
@@ -11,18 +11,7 @@ pub type TxNonce = u64;
 pub type BlockNonce = u64;
 pub type SenderAddress<M> = ManagedAddress<M>;
 pub type ReceiverAddress<M> = ManagedAddress<M>;
-pub type TxAsMultiValue<M> = MultiValue7<
-    BlockNonce,
-    TxNonce,
-    SenderAddress<M>,
-    ReceiverAddress<M>,
-    ManagedVec<M, EsdtTokenPayment<M>>,
-    ManagedVec<M, EsdtTokenData<M>>,
-    Option<TransferData<M>>,
->;
-pub type PaymentsVec<M> = ManagedVec<M, EsdtTokenPayment<M>>;
 pub type EventPaymentTuple<M> = MultiValue3<TokenIdentifier<M>, u64, EsdtTokenData<M>>;
-pub type TxBatchSplitInFields<M> = MultiValue2<BatchId, MultiValueEncoded<M, TxAsMultiValue<M>>>;
 pub type ExtractedFeeResult<M> =
     MultiValue2<OptionalValue<EsdtTokenPayment<M>>, ManagedVec<M, EsdtTokenPayment<M>>>;
 pub type OptionalValueTransferDataTuple<M> =
