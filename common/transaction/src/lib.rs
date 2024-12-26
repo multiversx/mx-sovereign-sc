17b7d918487653c9c6afec6b815f4f1f1c416d09
Andrei Baltariu <andreibaltariu1304@gmail.com>
2024-12-26 11:03:53 +0200

    Added file for aliases
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/transaction/src/lib.rs
+++ common/transaction/src/lib.rs
@@ -1,38 +1,18 @@
 #![no_std]
 
+use aliases::{
+    BlockNonce, EventPaymentTuple, GasLimit, OptionalValueTransferDataTuple, TxAsMultiValue, TxId,
+    TxNonce,
+};
+
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
+pub mod aliases;
 pub mod transaction_status;
 
 // revert protection
 pub const MIN_BLOCKS_FOR_FINALITY: u64 = 10;
-
-pub type BatchId = u64;
-pub type TxId = u64;
-pub type GasLimit = u64;
-pub type TxNonce = u64;
-
-pub type BlockNonce = u64;
-pub type SenderAddress<M> = ManagedAddress<M>;
-pub type ReceiverAddress<M> = ManagedAddress<M>;
-pub type TxAsMultiValue<M> = MultiValue7<
-    BlockNonce,
-    TxNonce,
-    SenderAddress<M>,
-    ReceiverAddress<M>,
-    ManagedVec<M, EsdtTokenPayment<M>>,
-    ManagedVec<M, EsdtTokenData<M>>,
-    Option<TransferData<M>>,
->;
-pub type PaymentsVec<M> = ManagedVec<M, EsdtTokenPayment<M>>;
-pub type EventPaymentTuple<M> = MultiValue3<TokenIdentifier<M>, u64, EsdtTokenData<M>>;
-pub type TxBatchSplitInFields<M> = MultiValue2<BatchId, MultiValueEncoded<M, TxAsMultiValue<M>>>;
-pub type ExtractedFeeResult<M> =
-    MultiValue2<OptionalValue<EsdtTokenPayment<M>>, ManagedVec<M, EsdtTokenPayment<M>>>;
-pub type OptionalValueTransferDataTuple<M> =
-    OptionalValue<MultiValue3<GasLimit, ManagedBuffer<M>, ManagedVec<M, ManagedBuffer<M>>>>;
-pub type StakeMultiArg<M> = MultiValue2<TokenIdentifier<M>, BigUint<M>>;
 
 #[type_abi]
 #[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, ManagedVecItem, Clone)]
