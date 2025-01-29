e84a8d4302d80c0643e83bba59290160086b0a92
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-01-29 15:37:10 +0200

    Added init and first mapper for `from-sovereign` sc
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- from-sovereign/src/lib.rs
+++ from-sovereign/src/lib.rs
@@ -2,11 +2,16 @@
 
 #[allow(unused_imports)]
 use multiversx_sc::imports::*;
+use operation::SovereignConfig;
+
+pub mod deposit;
 
 #[multiversx_sc::contract]
-pub trait FromSovereign {
+pub trait FromSovereign: deposit::DepositModule + cross_chain::CrossChainCommon {
     #[init]
-    fn init(&self) {}
+    fn init(&self, sovereign_config: SovereignConfig<Self::Api>) {
+        self.sovereign_config().set(sovereign_config);
+    }
 
     #[upgrade]
     fn upgrade(&self) {}
