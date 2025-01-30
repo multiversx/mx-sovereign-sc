7a509fe414c0da7053ff5984f16351115c97c1cf
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-01-30 14:59:23 +0200

    Added events module
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

---
+++ common/cross-chain/src/events.rs
@@ -0,0 +1,15 @@
+use operation::OperationData;
+
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+#[multiversx_sc::module]
+pub trait EventsModule {
+    #[event("deposit")]
+    fn deposit_event(
+        &self,
+        #[indexed] dest_address: &ManagedAddress,
+        #[indexed] tokens: &MultiValueEncoded<MultiValue3<TokenIdentifier, u64, EsdtTokenData>>,
+        event_data: OperationData<Self::Api>,
+    );
+}
