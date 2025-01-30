cb73d3bdee7c5a2cabd6f8a244eb373869e6ab08
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-01-30 15:59:01 +0200

    Moved functions
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/cross-chain/src/lib.rs
+++ common/cross-chain/src/lib.rs
@@ -1,14 +1,71 @@
 #![no_std]
 
-use operation::aliases::{GasLimit, TxNonce};
+use multiversx_sc::storage::StorageKey;
+use operation::{
+    aliases::{ExtractedFeeResult, GasLimit, TxNonce},
+    TransferData,
+};
+use proxies::fee_market_proxy::FeeMarketProxy;
 
 pub mod events;
 pub mod storage;
 
+pub const MAX_TRANSFERS_PER_TX: usize = 10;
+
 multiversx_sc::imports!();
 
 #[multiversx_sc::module]
-pub trait CrossChainCommon: crate::storage::CrossChainStorage {
+pub trait CrossChainCommon: crate::storage::CrossChainStorage + utils::UtilsModule {
+    fn match_fee_payment(
+        &self,
+        total_tokens_for_fees: usize,
+        fees_payment: &OptionalValue<EsdtTokenPayment<Self::Api>>,
+        opt_transfer_data: &Option<TransferData<<Self as ContractBase>::Api>>,
+    ) {
+        match fees_payment {
+            OptionalValue::Some(fee) => {
+                let mut gas: GasLimit = 0;
+
+                if let Some(transfer_data) = opt_transfer_data {
+                    gas = transfer_data.gas_limit;
+                }
+
+                let fee_market_address = self.fee_market_address().get();
+                let caller = self.blockchain().get_caller();
+
+                self.tx()
+                    .to(fee_market_address)
+                    .typed(FeeMarketProxy)
+                    .subtract_fee(caller, total_tokens_for_fees, OptionalValue::Some(gas))
+                    .payment(fee.clone())
+                    .sync_call();
+            }
+            OptionalValue::None => (),
+        };
+    }
+
+    fn check_and_extract_fee(&self) -> ExtractedFeeResult<Self::Api> {
+        let payments = self.call_value().all_esdt_transfers().clone();
+
+        require!(!payments.is_empty(), "Nothing to transfer");
+        require!(payments.len() <= MAX_TRANSFERS_PER_TX, "Too many tokens");
+
+        let fee_market_address = self.fee_market_address().get();
+        let fee_enabled_mapper = SingleValueMapper::new_from_address(
+            fee_market_address.clone(),
+            StorageKey::from("feeEnabledFlag"),
+        )
+        .get();
+
+        let opt_transfer_data = if fee_enabled_mapper {
+            OptionalValue::Some(self.pop_first_payment(payments.clone()).0)
+        } else {
+            OptionalValue::None
+        };
+
+        MultiValue2::from((opt_transfer_data, payments))
+    }
+
     #[inline]
     fn require_token_not_on_blacklist(&self, token_id: &TokenIdentifier) {
         require!(
