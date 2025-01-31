86ce50d5fd379a05cbe6709d4356adf1e8e4faa0
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-01-31 12:42:58 +0200

    Moved storage to common storage module
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/cross-chain/src/storage.rs
+++ common/cross-chain/src/storage.rs
@@ -1,4 +1,4 @@
-use operation::{aliases::TxNonce, CrossChainConfig};
+use operation::{aliases::TxNonce, CrossChainConfig, EsdtInfo};
 
 multiversx_sc::imports!();
 
@@ -12,4 +12,33 @@ pub trait CrossChainStorage {
 
     #[storage_mapper("feeMarketAddress")]
     fn fee_market_address(&self) -> SingleValueMapper<ManagedAddress>;
+
+    #[storage_mapper("sovToMxTokenId")]
+    fn sovereign_to_multiversx_token_id_mapper(
+        &self,
+        sov_token_id: &TokenIdentifier,
+    ) -> SingleValueMapper<TokenIdentifier>;
+
+    #[storage_mapper("mxToSovTokenId")]
+    fn multiversx_to_sovereign_token_id_mapper(
+        &self,
+        mx_token_id: &TokenIdentifier,
+    ) -> SingleValueMapper<TokenIdentifier>;
+
+    #[storage_mapper("sovEsdtTokenInfoMapper")]
+    fn sovereign_to_multiversx_esdt_info_mapper(
+        &self,
+        token_identifier: &TokenIdentifier,
+        nonce: u64,
+    ) -> SingleValueMapper<EsdtInfo<Self::Api>>;
+
+    #[storage_mapper("mxEsdtTokenInfoMapper")]
+    fn multiversx_to_sovereign_esdt_info_mapper(
+        &self,
+        token_identifier: &TokenIdentifier,
+        nonce: u64,
+    ) -> SingleValueMapper<EsdtInfo<Self::Api>>;
+
+    #[storage_mapper("isSovereignChain")]
+    fn is_sovereign_chain(&self) -> SingleValueMapper<bool>;
 }
