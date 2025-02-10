ea5bafc06b00a256cc9dd91374bf3a913fb1da05
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-02-10 11:58:12 +0200

    Added 2 more tests
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- from-sovereign/tests/from_sovereign_unit_tests.rs
+++ from-sovereign/tests/from_sovereign_unit_tests.rs
@@ -1,17 +1,23 @@
 use cross_chain::{storage::CrossChainStorage, DEFAULT_ISSUE_COST};
-use multiversx_sc::types::{
-    BigUint, EsdtTokenType, ManagedBuffer, TestAddress, TestSCAddress, TestTokenIdentifier,
+use multiversx_sc::{
+    imports::OptionalValue,
+    types::{
+        BigUint, EsdtTokenPayment, EsdtTokenType, ManagedAddress, ManagedBuffer, TestAddress,
+        TestSCAddress, TestTokenIdentifier, TokenIdentifier,
+    },
 };
+use multiversx_sc_modules::transfer_role_proxy::PaymentsVec;
 use multiversx_sc_scenario::{
     api::StaticApi, imports::MxscPath, ReturnsHandledOrError, ScenarioTxRun, ScenarioTxWhitebox,
     ScenarioWorld,
 };
-use operation::CrossChainConfig;
+use operation::{aliases::OptionalValueTransferDataTuple, CrossChainConfig};
 use proxies::from_sovereign_proxy::FromSovereignProxy;
 
 const CONTRACT_ADDRESS: TestSCAddress = TestSCAddress::new("sc");
 const CONTRACT_CODE_PATH: MxscPath = MxscPath::new("output/from-sovereign.mxsc.json");
 const OWNER_ADDRESS: TestAddress = TestAddress::new("owner");
+const USER: TestAddress = TestAddress::new("user");
 
 const OWNER_BALANCE: u128 = 100_000_000_000_000_000_000_000;
 
@@ -33,6 +39,10 @@ impl FromSovereignTestState {
         world
             .account(OWNER_ADDRESS)
             .nonce(1)
+            .esdt_balance(
+                TokenIdentifier::from("test-token"),
+                BigUint::from(100_000_000u64),
+            )
             .balance(BigUint::from(OWNER_BALANCE));
 
         Self { world }
@@ -82,6 +92,34 @@ impl FromSovereignTestState {
             assert_eq!(error_message, Some(error.message.as_str()))
         }
     }
+
+    fn deposit(
+        &mut self,
+        to: ManagedAddress<StaticApi>,
+        opt_transfer_data: OptionalValueTransferDataTuple<StaticApi>,
+        opt_payment: Option<PaymentsVec<StaticApi>>,
+        error_message: Option<&str>,
+    ) {
+        let tx = self
+            .world
+            .tx()
+            .from(OWNER_ADDRESS)
+            .to(CONTRACT_ADDRESS)
+            .typed(FromSovereignProxy)
+            .deposit(to, opt_transfer_data);
+
+        let response = if let Some(payment) = opt_payment {
+            tx.payment(payment)
+                .returns(ReturnsHandledOrError::new())
+                .run()
+        } else {
+            tx.returns(ReturnsHandledOrError::new()).run()
+        };
+
+        if let Err(error) = response {
+            assert_eq!(error_message, Some(error.message.as_str()))
+        }
+    }
 }
 
 #[test]
@@ -103,7 +141,7 @@ fn register_token_not_enough_egld() {
     let token_display_name = ManagedBuffer::from("TTK");
     let token_ticker = ManagedBuffer::from("TTK");
     let num_decimals = 0 as usize;
-    let error_message = Some("eGLD value should be 0.05");
+    let error_message = Some("EGLD value should be 0.05");
 
     state.register_token(
         egld_amount,
@@ -218,4 +256,39 @@ fn register_token_nonfungible_token() {
                     == mvx_token_id
             );
         })
+}
+
+#[test]
+fn deposit_nothing_to_transfer() {
+    let mut state = FromSovereignTestState::new();
+
+    state.deploy_contract(CrossChainConfig::default_config());
+    state.deposit(
+        USER.to_managed_address(),
+        OptionalValue::None,
+        None,
+        Some("Nothing to transfer"),
+    );
+}
+
+#[test]
+fn deposit_too_many_tokens() {
+    let mut state = FromSovereignTestState::new();
+
+    state.deploy_contract(CrossChainConfig::default_config());
+
+    let esdt_token_payment = EsdtTokenPayment::<StaticApi>::new(
+        TokenIdentifier::from("test-token"),
+        0,
+        BigUint::default(),
+    );
+
+    let payments_vec = PaymentsVec::from(vec![esdt_token_payment; 11]);
+
+    state.deposit(
+        USER.to_managed_address(),
+        OptionalValue::None,
+        Some(payments_vec),
+        Some("Too many tokens"),
+    );
 }
