4ce68f14b32af56fbf24d050bb660ca2e118514a
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-02-18 11:28:42 +0200

    Added event in common events module
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/cross-chain/src/events.rs
+++ common/cross-chain/src/events.rs
@@ -12,4 +12,11 @@ pub trait EventsModule {
         #[indexed] tokens: &MultiValueEncoded<MultiValue3<TokenIdentifier, u64, EsdtTokenData>>,
         event_data: OperationData<Self::Api>,
     );
+
+    #[event("executedBridgeOp")]
+    fn execute_bridge_operation_event(
+        &self,
+        #[indexed] hash_of_hashes: &ManagedBuffer,
+        #[indexed] hash_of_bridge_op: &ManagedBuffer,
+    );
 }
