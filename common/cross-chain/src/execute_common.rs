b4beabe00d7405c0dfcb28c518bbc087c77951b7
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-02-18 12:04:35 +0200

    Added getter for header verifier address
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/cross-chain/src/execute_common.rs
+++ common/cross-chain/src/execute_common.rs
@@ -19,21 +19,29 @@ pub trait ExecuteCommonModule: crate::storage::CrossChainStorage {
     }
 
     fn lock_operation_hash(&self, operation_hash: &ManagedBuffer, hash_of_hashes: &ManagedBuffer) {
-        let header_verifier_address = self.header_verifier_address().get();
-
         self.tx()
-            .to(header_verifier_address)
+            .to(self.get_header_verifier_address())
             .typed(HeaderverifierProxy)
             .lock_operation_hash(hash_of_hashes, operation_hash)
             .sync_call();
     }
 
     fn remove_executed_hash(&self, hash_of_hashes: &ManagedBuffer, op_hash: &ManagedBuffer) {
-        let header_verifier_address = self.header_verifier_address().get();
         self.tx()
-            .to(header_verifier_address)
+            .to(self.get_header_verifier_address())
             .typed(HeaderverifierProxy)
             .remove_executed_hash(hash_of_hashes, op_hash)
             .sync_call();
+    }
+
+    fn get_header_verifier_address(&self) -> ManagedAddress {
+        let header_verifier_address_mapper = self.header_verifier_address();
+
+        require!(
+            !header_verifier_address_mapper.is_empty(),
+            "There is no Header-Verifier address registered"
+        );
+
+        header_verifier_address_mapper.get()
     }
 }
