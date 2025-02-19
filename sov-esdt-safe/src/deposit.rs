7cad3be0d533bc5a2196e34977617cd33fc7d54e
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-02-19 16:54:16 +0200

    Added sov-esdt-safe contract
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

---
+++ sov-esdt-safe/src/deposit.rs
@@ -0,0 +1,91 @@
+multiversx_sc::imports!();
+use operation::{aliases::OptionalValueTransferDataTuple, OperationData, TransferData};
+
+#[multiversx_sc::module]
+pub trait DepositModule:
+    multiversx_sc_modules::pause::PauseModule
+    + utils::UtilsModule
+    + cross_chain::deposit_common::DepositCommonModule
+    + cross_chain::execute_common::ExecuteCommonModule
+    + cross_chain::storage::CrossChainStorage
+    + cross_chain::events::EventsModule
+    + max_bridged_amount_module::MaxBridgedAmountModule
+{
+    #[payable]
+    #[endpoint]
+    fn deposit(
+        &self,
+        to: ManagedAddress,
+        opt_transfer_data: OptionalValueTransferDataTuple<Self::Api>,
+    ) {
+        require!(self.not_paused(), "Cannot create transaction while paused");
+
+        let (fees_payment, payments) = self.check_and_extract_fee().into_tuple();
+
+        let mut total_tokens_for_fees = 0usize;
+        let mut event_payments = MultiValueEncoded::new();
+        let mut refundable_payments = ManagedVec::<Self::Api, _>::new();
+
+        let own_sc_address = self.blockchain().get_sc_address();
+
+        for payment in &payments {
+            self.require_below_max_amount(&payment.token_identifier, &payment.amount);
+            self.require_token_not_on_blacklist(&payment.token_identifier);
+
+            if !self.is_token_whitelist_empty()
+                && !self.is_token_whitelisted(&payment.token_identifier)
+            {
+                refundable_payments.push(payment.clone());
+
+                continue;
+            } else {
+                total_tokens_for_fees += 1;
+            }
+
+            let mut current_token_data = self.blockchain().get_esdt_token_data(
+                &own_sc_address,
+                &payment.token_identifier,
+                payment.token_nonce,
+            );
+            current_token_data.amount = payment.amount.clone();
+
+            self.tx()
+                .to(ToSelf)
+                .typed(system_proxy::UserBuiltinProxy)
+                .esdt_local_burn(
+                    &payment.token_identifier,
+                    payment.token_nonce,
+                    &payment.amount,
+                )
+                .sync_call();
+
+            event_payments.push(MultiValue3::from((
+                payment.token_identifier.clone(),
+                payment.token_nonce,
+                current_token_data,
+            )));
+        }
+
+        let option_transfer_data = TransferData::from_optional_value(opt_transfer_data);
+
+        if let Some(transfer_data) = option_transfer_data.as_ref() {
+            self.require_gas_limit_under_limit(transfer_data.gas_limit);
+            self.require_endpoint_not_banned(&transfer_data.function);
+        }
+        self.match_fee_payment(total_tokens_for_fees, &fees_payment, &option_transfer_data);
+
+        // refund tokens
+        let caller = self.blockchain().get_caller();
+        self.tx()
+            .to(&caller)
+            .multi_esdt(refundable_payments)
+            .transfer();
+
+        let tx_nonce = self.get_and_save_next_tx_id();
+        self.deposit_event(
+            &to,
+            &event_payments,
+            OperationData::new(tx_nonce, caller, option_transfer_data),
+        );
+    }
+}
---
+++ sov-esdt-safe/src/lib.rs
@@ -0,0 +1,32 @@
+#![no_std]
+
+#[allow(unused_imports)]
+use multiversx_sc::imports::*;
+use operation::EsdtSafeConfig;
+
+pub mod deposit;
+
+#[multiversx_sc::contract]
+pub trait SovEsdtSafe:
+    cross_chain::deposit_common::DepositCommonModule
+    + cross_chain::execute_common::ExecuteCommonModule
+    + cross_chain::storage::CrossChainStorage
+    + utils::UtilsModule
+{
+    #[init]
+    fn init(&self, header_verifier_address: ManagedAddress, config: EsdtSafeConfig<Self::Api>) {
+        self.require_sc_address(&header_verifier_address);
+        self.header_verifier_address().set(&header_verifier_address);
+        self.esdt_safe_config().set(config);
+    }
+
+    #[only_owner]
+    #[endpoint(setFeeMarketAddress)]
+    fn set_fee_market_address(&self, fee_market_address: ManagedAddress) {
+        self.require_sc_address(&fee_market_address);
+        self.fee_market_address().set(fee_market_address);
+    }
+
+    #[upgrade]
+    fn upgrade(&self) {}
+}
