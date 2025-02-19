e1de806dbab6c4202cf98701c5f0132c01655ef3
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-02-19 16:54:39 +0200

    Added sov-esdt-safe toml
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/proxies/src/lib.rs
+++ common/proxies/src/lib.rs
@@ -5,6 +5,7 @@ pub mod enshrine_esdt_safe_proxy;
 pub mod esdt_safe_proxy;
 pub mod fee_market_proxy;
 pub mod header_verifier_proxy;
+pub mod sov_esdt_safe_proxy;
 pub mod sovereign_forge_proxy;
 pub mod testing_sc_proxy;
 pub mod to_sovereign_proxy;
