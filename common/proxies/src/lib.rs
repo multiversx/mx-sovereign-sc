9960006f41b0d651031a9221aed0513255f31820
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-03-03 14:22:20 +0200

    Removed esdt-safe proxy
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/proxies/src/lib.rs
+++ common/proxies/src/lib.rs
@@ -2,7 +2,6 @@
 pub mod chain_config_proxy;
 pub mod chain_factory_proxy;
 pub mod enshrine_esdt_safe_proxy;
-pub mod esdt_safe_proxy;
 pub mod fee_market_proxy;
 pub mod header_verifier_proxy;
 pub mod mvx_esdt_safe_proxy;
