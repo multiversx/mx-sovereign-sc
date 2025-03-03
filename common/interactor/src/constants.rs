dde7b9a4efcdf1b0ad613541f79ec71772488461
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-03-03 14:27:12 +0200

    Removed interactor module
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/interactor/src/constants.rs
+++
@@ -1,4 +0,0 @@
-pub const TOKEN_ID: &[u8] = b"SVT-805b28";
-pub const WHITELIST_TOKEN_ID: &[u8] = b"CHOCOLATE-daf625";
-pub const TOKEN_ID_FOR_EXECUTE: &[u8] = b"x-SOV-101252";
-pub const WHITELISTED_TOKEN_ID: &[u8] = b"CHOCOLATE-daf625";
