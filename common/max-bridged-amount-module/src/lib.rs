2519a132cca935345ea2cfaea2f0ca65183179e7
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-03-07 12:02:54 +0200

    Moved max bridged amount logic in common cross-chain module
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- common/max-bridged-amount-module/src/lib.rs
+++ common/max-bridged-amount-module/src/lib.rs
@@ -5,30 +5,4 @@ use error_messages::DEPOSIT_OVER_MAX_AMOUNT;
 multiversx_sc::imports!();
 
 #[multiversx_sc::module]
-pub trait MaxBridgedAmountModule {
-    #[only_owner]
-    #[endpoint(setMaxBridgedAmount)]
-    fn set_max_bridged_amount(&self, token_id: TokenIdentifier, max_amount: BigUint) {
-        self.max_bridged_amount(&token_id).set(&max_amount);
-    }
-
-    fn is_above_max_amount(&self, token_id: &TokenIdentifier, amount: &BigUint) -> bool {
-        let max_amount = self.max_bridged_amount(token_id).get();
-        if max_amount > 0 {
-            amount > &max_amount
-        } else {
-            false
-        }
-    }
-
-    fn require_below_max_amount(&self, token_id: &TokenIdentifier, amount: &BigUint) {
-        require!(
-            !self.is_above_max_amount(token_id, amount),
-            DEPOSIT_OVER_MAX_AMOUNT
-        );
-    }
-
-    #[view(getMaxBridgedAmount)]
-    #[storage_mapper("maxBridgedAmount")]
-    fn max_bridged_amount(&self, token_id: &TokenIdentifier) -> SingleValueMapper<BigUint>;
-}
+pub trait MaxBridgedAmountModule {}
