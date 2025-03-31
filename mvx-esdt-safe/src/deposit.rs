61dfcb0bfafb0434f3a6c8a3ef5531f566480c84
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-03-31 14:12:08 +0300

    `mvx-esdt-safe` deposit cleanup
    Signed-off-by: Andrei Baltariu <andreibaltariu1304@gmail.com>

--- mvx-esdt-safe/src/deposit.rs
+++ mvx-esdt-safe/src/deposit.rs
@@ -23,68 +23,60 @@ pub trait DepositModule:
     ) {
         require!(self.not_paused(), ESDT_SAFE_STILL_PAUSED);
 
-        let is_deposit_with_no_transfer = self.call_value().all_esdt_transfers().clone().is_empty()
-            && opt_transfer_data.is_some();
+        let option_transfer_data = TransferData::from_optional_value(&opt_transfer_data);
 
-        if is_deposit_with_no_transfer {
-            let option_transfer_data = TransferData::from_optional_value(&opt_transfer_data);
+        if let Some(transfer_data) = &option_transfer_data {
+            self.require_gas_limit_under_limit(transfer_data.gas_limit);
+            self.require_endpoint_not_banned(&transfer_data.function);
+        }
 
-            if let Some(transfer_data) = option_transfer_data.as_ref() {
-                self.require_gas_limit_under_limit(transfer_data.gas_limit);
-                self.require_endpoint_not_banned(&transfer_data.function);
-            }
+        let has_no_esdt_transfers = self.call_value().all_esdt_transfers().clone().is_empty();
+        let deposit_without_transfers = has_no_esdt_transfers && opt_transfer_data.is_some();
 
+        if deposit_without_transfers {
             let caller = self.blockchain().get_caller();
-
             let tx_nonce = self.get_and_save_next_tx_id();
             self.deposit_event(
                 &to,
                 &MultiValueEncoded::new(),
                 OperationData::new(tx_nonce, caller, option_transfer_data),
             );
-        } else {
-            let (fees_payment, payments) = self.check_and_extract_fee().into_tuple();
-
-            let mut total_tokens_for_fees = 0usize;
-            let mut event_payments = MultiValueEncoded::new();
-            let mut refundable_payments = ManagedVec::<Self::Api, _>::new();
-            let own_sc_address = self.blockchain().get_sc_address();
-
-            for payment in &payments {
-                self.require_below_max_amount(&payment.token_identifier, &payment.amount);
-                self.require_token_not_on_blacklist(&payment.token_identifier);
-
-                if !self.is_token_whitelist_empty()
-                    && !self.is_token_whitelisted(&payment.token_identifier)
-                {
-                    refundable_payments.push(payment.clone());
-                    continue;
-                }
-                total_tokens_for_fees += 1;
-
-                let processed_payment = self.process_payment(&payment, &own_sc_address);
-                event_payments.push(processed_payment);
-            }
-
-            let option_transfer_data = TransferData::from_optional_value(&opt_transfer_data);
-
-            if let Some(transfer_data) = option_transfer_data.as_ref() {
-                self.require_gas_limit_under_limit(transfer_data.gas_limit);
-                self.require_endpoint_not_banned(&transfer_data.function);
-            }
-
-            self.match_fee_payment(total_tokens_for_fees, &fees_payment, &option_transfer_data);
-
-            let caller = self.blockchain().get_caller();
-            self.refund_tokens(&caller, refundable_payments);
-
-            let tx_nonce = self.get_and_save_next_tx_id();
-            self.deposit_event(
-                &to,
-                &event_payments,
-                OperationData::new(tx_nonce, caller, option_transfer_data),
-            );
+            return;
         }
+
+        let (fees_payment, payments) = self.check_and_extract_fee().into_tuple();
+        let mut total_tokens_for_fees = 0usize;
+        let mut event_payments = MultiValueEncoded::new();
+        let mut refundable_payments = ManagedVec::<Self::Api, _>::new();
+        let own_sc_address = self.blockchain().get_sc_address();
+
+        for payment in &payments {
+            self.require_below_max_amount(&payment.token_identifier, &payment.amount);
+            self.require_token_not_on_blacklist(&payment.token_identifier);
+
+            if !self.is_token_whitelist_empty()
+                && !self.is_token_whitelisted(&payment.token_identifier)
+            {
+                refundable_payments.push(payment.clone());
+                continue;
+            }
+
+            total_tokens_for_fees += 1;
+            let processed_payment = self.process_payment(&payment, &own_sc_address);
+            event_payments.push(processed_payment);
+        }
+
+        self.match_fee_payment(total_tokens_for_fees, &fees_payment, &option_transfer_data);
+
+        let caller = self.blockchain().get_caller();
+        self.refund_tokens(&caller, refundable_payments);
+
+        let tx_nonce = self.get_and_save_next_tx_id();
+        self.deposit_event(
+            &to,
+            &event_payments,
+            OperationData::new(tx_nonce, caller, option_transfer_data),
+        );
     }
 
     fn process_payment(
