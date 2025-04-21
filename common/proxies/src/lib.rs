d49a1b31eb26005b796675966df51d6a3b4663c5
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-04-21 10:20:28 +0300

    Added enshrine-esdt-safe proxy

--- common/proxies/src/lib.rs
+++ common/proxies/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 pub mod chain_config_proxy;
 // pub mod chain_factory_proxy;
-// pub mod enshrine_esdt_safe_proxy;
+pub mod enshrine_esdt_safe_proxy;
 // pub mod esdt_safe_proxy;
 pub mod fee_market_proxy;
 pub mod header_verifier_proxy;
