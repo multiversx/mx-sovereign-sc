aa2f981c5e5bcf7880238c9440dbc1b0412feeeb
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-04-23 15:21:21 +0300

    Updated factory.rs file

--- chain-factory/src/factory.rs
+++ chain-factory/src/factory.rs
@@ -2,12 +2,12 @@ use multiversx_sc::imports::*;
 use multiversx_sc_modules::only_admin;
 use proxies::{
     chain_config_proxy::ChainConfigContractProxy,
-    // enshrine_esdt_safe_proxy::EnshrineEsdtSafeProxy,
-    // esdt_safe_proxy::EsdtSafeProxy,
+    enshrine_esdt_safe_proxy::EnshrineEsdtSafeProxy,
     fee_market_proxy::{FeeMarketProxy, FeeStruct},
     header_verifier_proxy::HeaderverifierProxy,
+    mvx_esdt_safe_proxy::MvxEsdtSafeProxy,
 };
-use structs::configs::SovereignConfig;
+use structs::configs::{EsdtSafeConfig, SovereignConfig};
 multiversx_sc::derive_imports!();
 
 #[multiversx_sc::module]
@@ -63,63 +63,64 @@ pub trait FactoryModule: only_admin::OnlyAdminModule {
             .sync_call();
     }
 
-    // #[only_admin]
-    // #[endpoint(deployEnshrineEsdtSafe)]
-    // fn deploy_enshrine_esdt_safe(
-    //     &self,
-    //     is_sovereign_chain: bool,
-    //     token_handler_address: ManagedAddress,
-    //     wegld_identifier: TokenIdentifier,
-    //     sov_token_prefix: ManagedBuffer,
-    //     opt_config: Option<EsdtSafeConfig<Self::Api>>,
-    // ) -> ManagedAddress {
-    //     let source_address = self.enshrine_esdt_safe_template().get();
-    //     let metadata = self.blockchain().get_code_metadata(&source_address);
-    //
-    //     self.tx()
-    //         .typed(EnshrineEsdtSafeProxy)
-    //         .init(
-    //             is_sovereign_chain,
-    //             token_handler_address,
-    //             Some(wegld_identifier),
-    //             Some(sov_token_prefix),
-    //             opt_config,
-    //         )
-    //         .gas(60_000_000)
-    //         .from_source(source_address)
-    //         .code_metadata(metadata)
-    //         .returns(ReturnsNewManagedAddress)
-    //         .sync_call()
-    // }
+    #[only_admin]
+    #[endpoint(deployEnshrineEsdtSafe)]
+    fn deploy_enshrine_esdt_safe(
+        &self,
+        is_sovereign_chain: bool,
+        token_handler_address: ManagedAddress,
+        wegld_identifier: TokenIdentifier,
+        sov_token_prefix: ManagedBuffer,
+        opt_config: Option<EsdtSafeConfig<Self::Api>>,
+    ) -> ManagedAddress {
+        let source_address = self.enshrine_esdt_safe_template().get();
+        let metadata = self.blockchain().get_code_metadata(&source_address);
 
-    // #[only_admin]
-    // #[endpoint(deployEsdtSafe)]
-    // fn deploy_esdt_safe(
-    //     &self,
-    //     is_sovereign_chain: bool,
-    //     header_verifier_address: ManagedAddress,
-    // ) -> ManagedAddress {
-    //     let source_address = self.enshrine_esdt_safe_template().get();
-    //     let metadata = self.blockchain().get_code_metadata(&source_address);
-    //
-    //     let esdt_safe_address = self
-    //         .tx()
-    //         .typed(EsdtSafeProxy)
-    //         .init(is_sovereign_chain)
-    //         .gas(60_000_000)
-    //         .from_source(source_address)
-    //         .code_metadata(metadata)
-    //         .returns(ReturnsNewManagedAddress)
-    //         .sync_call();
-    //
-    //     self.tx()
-    //         .to(header_verifier_address)
-    //         .typed(HeaderverifierProxy)
-    //         .set_esdt_safe_address(&esdt_safe_address)
-    //         .sync_call();
-    //
-    //     esdt_safe_address
-    // }
+        self.tx()
+            .typed(EnshrineEsdtSafeProxy)
+            .init(
+                is_sovereign_chain,
+                token_handler_address,
+                Some(wegld_identifier),
+                Some(sov_token_prefix),
+                opt_config,
+            )
+            .gas(60_000_000)
+            .from_source(source_address)
+            .code_metadata(metadata)
+            .returns(ReturnsNewManagedAddress)
+            .sync_call()
+    }
+
+    #[only_admin]
+    #[endpoint(deployEsdtSafe)]
+    fn deploy_mvx_esdt_safe(
+        &self,
+        header_verifier_address: ManagedAddress,
+        opt_config: OptionalValue<EsdtSafeConfig<Self::Api>>,
+    ) -> ManagedAddress {
+        let source_address = self.enshrine_esdt_safe_template().get();
+        let metadata = self.blockchain().get_code_metadata(&source_address);
+
+        let esdt_safe_address = self
+            .tx()
+            .typed(MvxEsdtSafeProxy)
+            .init(&header_verifier_address, opt_config)
+            .gas(60_000_000)
+            .from_source(source_address)
+            .code_metadata(metadata)
+            .returns(ReturnsNewManagedAddress)
+            .sync_call();
+
+        // TODO: mvx or sov ?
+        self.tx()
+            .to(header_verifier_address)
+            .typed(HeaderverifierProxy)
+            .set_esdt_safe_address(&esdt_safe_address)
+            .sync_call();
+
+        esdt_safe_address
+    }
 
     #[only_admin]
     #[endpoint(deployFeeMarket)]
@@ -131,20 +132,23 @@ pub trait FactoryModule: only_admin::OnlyAdminModule {
         let source_address = self.fee_market_template().get();
         let metadata = self.blockchain().get_code_metadata(&source_address);
 
-        self.tx()
+        let fee_market_address = self
+            .tx()
             .typed(FeeMarketProxy)
             .init(&esdt_safe_address, fee)
             .gas(60_000_000)
             .from_source(source_address)
             .code_metadata(metadata)
             .returns(ReturnsNewManagedAddress)
-            .sync_call()
+            .sync_call();
 
-        // self.tx()
-        //     .to(&esdt_safe_address)
-        //     .typed(EsdtSafeProxy)
-        //     .set_fee_market_address(&fee_market_address)
-        //     .sync_call();
+        self.tx()
+            .to(&esdt_safe_address)
+            .typed(MvxEsdtSafeProxy)
+            .set_fee_market_address(&fee_market_address)
+            .sync_call();
+
+        fee_market_address
     }
 
     // TODO:
