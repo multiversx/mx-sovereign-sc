8afd2b6609a5224f4311d4bf1775fc4da762ba0c
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-05-05 18:26:02 +0300

    Refactor

--- sovereign-forge/src/update_configs.rs
+++ sovereign-forge/src/update_configs.rs
@@ -1,4 +1,3 @@
-use multiversx_sc::require;
 use proxies::chain_factory_proxy::ChainFactoryContractProxy;
 use structs::configs::{EsdtSafeConfig, SovereignConfig};
 
@@ -13,17 +12,13 @@ pub trait UpdateConfigsModule: common::utils::UtilsModule + common::storage::Sto
 
         self.require_phase_three_completed(&caller);
 
-        require!(
-            !self.is_contract_deployed(&caller, ScArray::FeeMarket),
-            "The Fee-Market SC is already deployed"
-        );
-
-        let esdt_safe_address = self.get_contract_address(&caller, ScArray::ESDTSafe);
-
         self.tx()
             .to(self.get_chain_factory_address())
             .typed(ChainFactoryContractProxy)
-            .update_esdt_safe_config(esdt_safe_address, new_config)
+            .update_esdt_safe_config(
+                self.get_contract_address(&caller, ScArray::ESDTSafe),
+                new_config,
+            )
             .sync_call();
     }
 
@@ -33,17 +28,14 @@ pub trait UpdateConfigsModule: common::utils::UtilsModule + common::storage::Sto
         let caller = blockchain_api.get_caller();
 
         self.require_phase_one_completed(&caller);
-        require!(
-            !self.is_contract_deployed(&caller, ScArray::HeaderVerifier),
-            "The Header-Verifier contract is already deployed"
-        );
-
-        let chain_config_address = self.get_contract_address(&caller, ScArray::ChainConfig);
 
         self.tx()
             .to(self.get_chain_factory_address())
             .typed(ChainFactoryContractProxy)
-            .update_sovereign_config(chain_config_address, new_config)
+            .update_sovereign_config(
+                self.get_contract_address(&caller, ScArray::ChainConfig),
+                new_config,
+            )
             .sync_call();
     }
 }
