bea6910e70a5f5ee225dfcd0bd75bbb7587bff30
Sergiu Osvat <sergiuosvat@yahoo.com>
2025-05-14 10:59:06 +0300

    add chain config unit test docs

--- chain-config/tests/chain_config_blackbox_tests.rs
+++ chain-config/tests/chain_config_blackbox_tests.rs
@@ -13,6 +13,14 @@ fn test_deploy_chain_config() {
     state.common_setup.deploy_chain_config(config);
 }
 
+/// ### TEST
+/// C_CONFIG-UPDATE_CONFIG-OK-001
+///
+/// ### ACTION
+/// Call 'update_chain_config()' with a valid new config
+///
+/// ### EXPECTED
+/// Chain config is updated with the new config
 #[test]
 fn test_update_config() {
     let mut state = ChainConfigTestState::new();
@@ -25,6 +33,14 @@ fn test_update_config() {
     state.update_chain_config(new_config, None);
 }
 
+/// ### TEST
+/// C_CONFIG-UPDATE_CONFIG-FAIL-001
+///
+/// ### ACTION
+/// Call 'update_chain_config()' with an invalid new config
+///
+/// ### EXPECTED
+/// Error INVALID_MIN_MAX_VALIDATOR_NUMBERS
 #[test]
 fn test_update_config_wrong_validators_array() {
     let mut state = ChainConfigTestState::new();
@@ -37,6 +53,14 @@ fn test_update_config_wrong_validators_array() {
     state.update_chain_config(new_config, Some(INVALID_MIN_MAX_VALIDATOR_NUMBERS));
 }
 
+/// ### TEST
+/// C_CONFIG-COMPLETE_SETUP_PHASE-OK-001
+///
+/// ### ACTION
+/// Call 'complete_chain_config_setup_phase()'
+///
+/// ### EXPECTED
+/// Chain config's setup phase is completed
 #[test]
 fn test_complete_setup_phase() {
     let mut state = ChainConfigTestState::new();
