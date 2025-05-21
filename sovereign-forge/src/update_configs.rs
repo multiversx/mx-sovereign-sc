82cd951a2f51cfa2e264b226b55fe79132378f59
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-05-21 11:38:02 +0300

    Added more sovereign config update options through sovereign-forge

--- sovereign-forge/src/update_configs.rs
+++ sovereign-forge/src/update_configs.rs
@@ -1,5 +1,7 @@
+use multiversx_sc::types::TokenIdentifier;
 use proxies::chain_factory_proxy::ChainFactoryContractProxy;
 use structs::configs::{EsdtSafeConfig, SovereignConfig};
+use structs::fee::FeeStruct;
 
 use crate::common::{self, utils::ScArray};
 use crate::err_msg;
@@ -35,6 +37,40 @@ pub trait UpdateConfigsModule: common::utils::UtilsModule + common::storage::Sto
             .update_sovereign_config(
                 self.get_contract_address(&caller, ScArray::ChainConfig),
                 new_config,
+            )
+            .sync_call();
+    }
+
+    #[endpoint(setFee)]
+    fn set_fee(&self, new_fee: FeeStruct<Self::Api>) {
+        let blockchain_api = self.blockchain();
+        let caller = blockchain_api.get_caller();
+
+        self.require_phase_four_completed(&caller);
+
+        self.tx()
+            .to(self.get_chain_factory_address())
+            .typed(ChainFactoryContractProxy)
+            .set_fee(
+                self.get_contract_address(&caller, ScArray::FeeMarket),
+                new_fee,
+            )
+            .sync_call();
+    }
+
+    #[endpoint(removeFee)]
+    fn remove_fee(&self, token_id: TokenIdentifier<Self::Api>) {
+        let blockchain_api = self.blockchain();
+        let caller = blockchain_api.get_caller();
+
+        self.require_phase_four_completed(&caller);
+
+        self.tx()
+            .to(self.get_chain_factory_address())
+            .typed(ChainFactoryContractProxy)
+            .remove_fee(
+                self.get_contract_address(&caller, ScArray::FeeMarket),
+                token_id,
             )
             .sync_call();
     }
