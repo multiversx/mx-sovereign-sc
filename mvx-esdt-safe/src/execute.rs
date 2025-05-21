e425abcd7449b7130005a66e47be9eade242aa84
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-05-21 17:43:07 +0300

    Added use of GenerateHash trait

--- mvx-esdt-safe/src/execute.rs
+++ mvx-esdt-safe/src/execute.rs
@@ -1,6 +1,7 @@
 use error_messages::ESDT_SAFE_STILL_PAUSED;
 use structs::{
     aliases::GasLimit,
+    generate_hash::GenerateHash,
     operation::{Operation, OperationData, OperationEsdtPayment, OperationTuple},
 };
 
@@ -25,7 +26,7 @@ pub trait ExecuteModule:
         require!(self.not_paused(), ESDT_SAFE_STILL_PAUSED);
         self.require_setup_complete();
 
-        let operation_hash = self.calculate_operation_hash(&operation);
+        let operation_hash = operation.generate_hash();
 
         self.lock_operation_hash(&operation_hash, &hash_of_hashes);
 
