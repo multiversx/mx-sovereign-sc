b4cbea59b10f13617cbc6c69dd0f00b6e18f3307
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-08-27 13:58:24 +0300

    Added setFee endpoint

--- sov-registrar/src/fee_operations.rs
+++ sov-registrar/src/fee_operations.rs
@@ -1,12 +1,21 @@
-use structs::fee::FeeStruct;
+use structs::fee::{FeeStruct, SetFeeOperation};
 multiversx_sc::imports!();
 
 #[multiversx_sc::module]
-pub trait FeeOperationsModule {
+pub trait FeeOperationsModule: tx_nonce::TxNonceModule + custom_events::CustomEventsModule {
     #[endpoint(registerSetFee)]
     fn register_set_fee(&self, fee_struct: FeeStruct<Self::Api>) {
-        if let Some(set_fee_error_msg) = self.set_fee_in_storage(&fee_struct) {
-            sc_panic!(set_fee_error_msg);
-        }
+        self.set_fee_event(SetFeeOperation {
+            fee_struct,
+            nonce: self.get_and_save_next_tx_id(),
+        });
     }
+
+    // #[endpoint(registerSetFee)]
+    // fn register_set_fee(&self, fee_struct: FeeStruct<Self::Api>) {
+    //     self.set_fee_event(SetFeeOperation {
+    //         fee_struct,
+    //         nonce: self.get_and_save_next_tx_id(),
+    //     });
+    // }
 }
